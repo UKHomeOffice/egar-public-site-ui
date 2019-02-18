e7229f42cf5cdf73e6943a1dfe8a437936b7ff33
James Lizamore <jwlizamore@gmail.com>
2019-02-18 16:33:43 +0000

    Update manifest page to match prototype
    Add bulk add functionality

--- src/app/garfile/manifest/post.controller.js
+++ src/app/garfile/manifest/post.controller.js
@@ -2,23 +2,33 @@ const logger = require('../../../common/utils/logger');
 const CookieModel = require('../../../common/models/Cookie.class');
 const { Manifest } = require('../../../common/models/Manifest.class');
 const garApi = require('../../../common/services/garApi');
+const manifestUtil = require('./bulkAdd');
 
 module.exports = (req, res) => {
   const cookie = new CookieModel(req);
   logger.debug('In garfile / manifest post controller');
   if (req.body.editPersonId) {
     req.session.editPersonId = req.body.editPersonId;
-    res.redirect('/garfile/manifest/editperson');
+    req.session.save(() => { res.redirect('/garfile/manifest/editperson'); });
   } else if (req.body.deletePersonId) {
     req.session.deletePersonId = req.body.deletePersonId;
-    res.redirect('/garfile/manifest/deleteperson');
-  } else if (req.body.addPersonId) {
-    req.session.addPersonId = req.body.addPersonId;
-    res.redirect('/garfile/manifest/addnewperson');
+    req.session.save(() => { res.redirect('/garfile/manifest/deleteperson'); });
+  } else if (req.body.personId) {
+    logger.debug('Found people to add to manifest');
+    manifestUtil.bulkAdd(req.body.personId, cookie.getUserDbId())
+      .then((selectedPeople) => {
+        garApi.patch(cookie.getGarId(), 'Draft', { people: selectedPeople })
+          .then(() => {
+            res.redirect('/garfile/manifest');
+          })
+          .catch(() => {
+            logger.info('Failed to patch GAR with updated manifest');
+            res.redirect('/garfile/manifest');
+          });
+      });
   } else if (req.body.buttonClicked === 'Save and Exit') {
     res.redirect('/garfile/manifest');
-  }
-    else if (req.body.buttonClicked === 'Save and Continue') {
+  } else if (req.body.buttonClicked === 'Save and Continue') {
     // Perform manifest validation then redirect to next section
     garApi.getPeople(cookie.getGarId())
       .then((apiResponse) => {
@@ -37,5 +47,7 @@ module.exports = (req, res) => {
         req.session.manifestErr = 'Failed to get manifest';
         return res.redirect('/garfile/manifest');
       });
+  } else {
+    return res.redirect('/garfile/manifest');
   }
 };
