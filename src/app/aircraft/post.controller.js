d27332b36fe8a7359115f2f037a92196b2aae9bb
joe <joseph.l.pike@gmail.com>
2019-02-21 12:38:16 +0000

    accounted for req sessionn redirect

--- src/app/aircraft/post.controller.js
+++ src/app/aircraft/post.controller.js
@@ -5,9 +5,9 @@ module.exports = (req, res) => {
   logger.debug('In user aircraft post controller');
   if (req.body.editCraft) {
     req.session.editCraftId = req.body.editCraft;
-    res.redirect('/aircraft/edit');
+    return req.session.save(() => {res.redirect('/aircraft/edit')});
   } else if (req.body.deleteCraft) {
     req.session.deleteCraftId = req.body.deleteCraft;
-    res.redirect('/aircraft/delete');
+    return req.session.save(() => {res.redirect('/aircraft/delete')});
   }
 }
