5967ca4d19de14b51ce02b835840cc97774a90df
James Lizamore <jwlizamore@gmail.com>
2019-04-15 10:50:36 +0100

    Add unavailable page, redirect reqs to it

--- src/app/unavailable/get.controller.js
+++ src/app/unavailable/get.controller.js
@@ -2,7 +2,7 @@ const logger = require('../../common/utils/logger');
 const CookieModel = require('../../common/models/Cookie.class');
 
 module.exports = (req, res) => {
-  logger.debug('In welcome get controller');
+  logger.debug('In unavailable get controller');
   const cookie = new CookieModel(req);
   res.render('app/unavailable/index', { cookie });
 };
