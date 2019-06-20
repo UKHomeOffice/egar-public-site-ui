160a8f25dbbd650dda1a7f01d3caba50593ec798
David Nguyen <david.nguyen@leidos.com>
2019-06-20 16:44:18 +0100

    Add session table script
    README highlighted that a table needed to be created manually, which is now not necesary, leveraging sequelize to create a table and updating it appropriately.
    
    Added a couple more unit tests for trivial get controllers.

--- src/app/user/manageuserdetail/get.controller.js
+++ src/app/user/manageuserdetail/get.controller.js
@@ -1,6 +1,5 @@
 const CookieModel = require('../../../common/models/Cookie.class');
 
-
 module.exports = (req, res) => {
   const cookie = new CookieModel(req);
   res.render('app/user/manageuserdetail/index', { cookie });
