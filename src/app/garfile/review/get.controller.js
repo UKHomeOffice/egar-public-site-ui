7972650fe935b29f5eb13b85e9e28e5694592239
Chun Chan <chun.chan@leidos.com>
2019-06-27 16:36:49 +0100

    Add validation rule on Section 8 Gar Review page
    -After gardetails had been retrieved, compare departure and arrival dates

--- src/app/garfile/review/get.controller.js
+++ src/app/garfile/review/get.controller.js
@@ -2,57 +2,70 @@ const logger = require('../../../common/utils/logger')(__filename);
 const CookieModel = require('../../../common/models/Cookie.class');
 const manifestFields = require('../../../common/seeddata/gar_manifest_fields.json');
 const garApi = require('../../../common/services/garApi');
+const ValidationRule = require('../../../common/models/ValidationRule.class');
+const validator = require('../../../common/utils/validator');
+
+const voyageDateMsg = 'Arrival date must not be earlier than departure date';
+
+function handleResponseError(parsedApiResponse) {
+  if ({}.hasOwnProperty.call(parsedApiResponse, 'message')) {
+    logger.debug(`Api returned Error: ${parsedApiResponse}`);
+  }
+}
 
 module.exports = (req, res) => {
   const cookie = new CookieModel(req);
   logger.debug('In garfile / review get controller');
 
-   garApi.getPeople(cookie.getGarId() )
-   .then((apiResponse) => {
-    const parsedResponse = JSON.parse(apiResponse);
-    if (parsedResponse.hasOwnProperty('message')) {
-    // API returned error
-      logger.debug(`Api returned Error: ${parsedResponse}`);
-    }
-    else{
-      const garpeople = JSON.parse(apiResponse);
-      garApi.get(cookie.getGarId())//get the gar
-      .then((apiResponse) => {
-        const parsedResponse = JSON.parse(apiResponse);
-        if (parsedResponse.hasOwnProperty('message')) {
-        // API returned error
-          logger.debug(`Api returned Error: ${parsedResponse}`);
-        }
-        else{
-          const garfile = JSON.parse(apiResponse);
-          garApi.getSupportingDocs(cookie.getGarId())
-          .then((apiResponse) => {
-            const parsedResponse = JSON.parse(apiResponse);
-            if (parsedResponse.hasOwnProperty('message')) {
-              // API returned error
-                logger.debug(`Api returned Error: ${parsedResponse}`);
-              }
-              else{
-                const supportingdocs = JSON.parse(apiResponse);
-                if (req.session.submiterrormessage && req.session.submiterrormessage.length > 0) {
-                  let msg = req.session.submiterrormessage;
-                  delete req.session.submiterrormessage
-                  res.render('app/garfile/review/index', {cookie,
-                    errors: msg,
-                    showChangeLinks: true,
-                    manifestFields, garfile,garpeople, supportingdocs});
-                  }
-                  else{
-                    res.render('app/garfile/review/index', {cookie,
-                      showChangeLinks: true,
-                      manifestFields,
-                      garfile,garpeople,
-                      supportingdocs});
-                  }
-              }
-            });
+  const garId = cookie.getGarId();
+
+  Promise.all([
+    garApi.getPeople(garId),
+    garApi.get(garId),
+    garApi.getSupportingDocs(garId)])
+    .then((apiResponse) => {
+      const garpeople = JSON.parse(apiResponse[0]);
+      handleResponseError(garpeople);
+
+      const garfile = JSON.parse(apiResponse[1]);
+      handleResponseError(garfile);
+
+      const garsupportingdocs = JSON.parse(apiResponse[2]);
+      handleResponseError(garsupportingdocs);
+
+      const voyageDateObj = {
+        departureDate: garfile.departureDate,
+        arrivalDate: garfile.arrivalDate,
+      };
+
+      const validations = [[
+        new ValidationRule(validator.isValidDepAndArrDate, 'voyageDates', voyageDateObj, voyageDateMsg),
+      ]];
+
+      const localvar = {
+        cookie,
+        showChangeLinks: true,
+        manifestFields,
+        garfile,
+        garpeople,
+        garsupportingdocs,
+      };
+
+      validator.validateChains(validations)
+        .then(() => {
+          if (req.session.submiterrormessage && req.session.submiterrormessage.length > 0) {
+            const msg = req.session.submiterrormessage;
+            delete req.session.submiterrormessage;
+            localvar.errors = msg;
+            res.render('app/garfile/review/index', localvar);
+          } else {
+            res.render('app/garfile/review/index', localvar);
           }
+        }).catch((err) => {
+          logger.info('Validation failed');
+          logger.info(err);
+          localvar.errors = err;
+          res.render('app/garfile/review/index', localvar);
         });
-      }
     });
-  }
+};
