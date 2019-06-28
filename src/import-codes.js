079b5fe0801740ba0c8e2a24a783e86f3d747379
David Nguyen <david.nguyen@leidos.com>
2019-06-28 11:20:16 +0100

    Implement port code converter
    Taking the data from openflights.org for airports, a small piece of code to convert it into a form that could be used by sGAR, allowing potentially a user to search via IATA or ICAO codes for an airport.
    
    It may be beneficial for data quality to prioritise ICAO codes in situations where IATA is used, this will be up for discussion.

---
+++ src/import-codes.js
@@ -0,0 +1,54 @@
+const csv = require('csvtojson/v2');
+const csvFile='./airports.dat';
+const fs = require('fs');
+
+/**
+ * A converter to take in an openflights.org data file (in CSV format) and converting it into a JSON format that can be
+ * used by sGAR.
+ *
+ * This will not be expected to be used during the app use and is expected to be run as:
+ * 
+ * node import-codes
+ * 
+ * Which will take a file called "airports.dat" which can be obtained via:
+ * https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat
+ * 
+ * So Kudos to openflights: https://openflights.org/data.html
+ */
+csv({
+  headers: ['id', 'name', 'city', 'country', 'IATA', 'ICAO', 'lat', 'long', 'alt', 'utc', 'dst', 'tz', 'type', 'source'],
+  colParser: {
+    'city': 'omit',
+    'lat': 'omit',
+    'long': 'omit',
+    'alt': 'omit',
+    'utc': 'omit',
+    'dst': 'omit',
+    'tz': 'omit',
+    'type': 'omit',
+    'source': 'omit',
+  },
+}).fromFile(csvFile).then((jsonResult) => {
+  let processedArray = [];
+  jsonResult.forEach((row) => {
+    console.log('Processing row ' + row.id + ' - ' + row.name);
+    console.log('IATA: ' + row.IATA);
+    console.log('ICAO: ' + row.ICAO);
+    const label = row.name + ' (' + row.country + ') ';
+    // It is possible that IATA codes do not exist, which appear to be read as a "\N" character so ignore those.
+    if (row.IATA !== '\\N') {
+      processedArray.push({id: row.IATA, label: label + '(' + row.IATA + ')'})
+    }
+    if (row.ICAO !== '\\N') {
+      processedArray.push({id: row.ICAO, label: label + '(' + row.ICAO + ')'})
+    }
+  });
+  console.log('Resulting output');
+  console.log(processedArray);
+  fs.writeFileSync('airport_codes.json', JSON.stringify(processedArray, null, 2), 'utf8', (err) => {
+    if (err) {
+      console.log('An error occurred while saving to JSON');
+      return console.log(err);
+    }
+  })
+})
