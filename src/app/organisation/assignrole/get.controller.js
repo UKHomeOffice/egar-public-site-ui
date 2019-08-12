e320ca791b3cd9475ae69c4f9a276cd1917e882d
David Nguyen <david.nguyen@leidos.com>
2019-08-12 13:21:36 +0100

    Add screen reader tweaks for organisation screens
    Create, assign role and edit pages gone through the a11y tweaks. Externalised strings also.
    Changed some variables like "orgname" and "Orgname" to be "orgName" for consistency.
    
    Updated i18n-iso-countries library from 4.2.1 to 4.3.0
    
    Fix typo in the role description for Users.
    
    Updated unit tests.

--- src/app/organisation/assignrole/get.controller.js
+++ src/app/organisation/assignrole/get.controller.js
@@ -1,8 +1,8 @@
 
-const cookieModel = require('../../../common/models/Cookie.class')
-const roles = require ('../../../common/seeddata/egar_user_roles.json')
+const CookieModel = require('../../../common/models/Cookie.class');
+const roles = require('../../../common/seeddata/egar_user_roles.json');
 
 module.exports = (req, res) => {
-  var cookie = new cookieModel(req);
-  res.render('app/organisation/assignrole/index', { cookie , roles });
+  const cookie = new CookieModel(req);
+  res.render('app/organisation/assignrole/index', { cookie, roles });
 };
