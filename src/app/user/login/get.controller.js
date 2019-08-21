ee10a79e574a3aaac93f2d825e1d5f44f80cd429
David Nguyen <david.nguyen@leidos.com>
2019-08-21 14:24:35 +0100

    Sonarqube driven development
    Upload file controller has a code chunk separated to reduce main function size.
    
    Used refactorings for the index.js for a few more files.
    
    Improved readability of the user login get controller by creating a function to separate it out.

--- src/app/user/login/get.controller.js
+++ src/app/user/login/get.controller.js
@@ -1,7 +1,20 @@
 const navUtil = require('../../../common/utils/nav');
 
+/**
+ * If incoming object contains a dbId (id), vr (verified) and rl (role),
+ * then consider the user logged in.
+ *
+ * @param {Object} userSessionObject object representing the User in the session.
+ */
+const checkUserInSession = (userSessionObject) => {
+  if (userSessionObject) {
+    return userSessionObject.dbId && userSessionObject.vr && userSessionObject.rl;
+  }
+  return false;
+};
+
 module.exports = (req, res) => {
-  if (req.headers.referer && req.session.u && req.session.u.dbId && req.session.u.vr && req.session.u.rl) {
+  if (req.headers.referer && req.session.u && checkUserInSession(req.session.u)) {
     res.redirect('/home');
     return;
   }
