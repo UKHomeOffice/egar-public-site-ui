6dfbb77357a5e1e90eeb4261d22ebc5a84cd91c0
David Nguyen <david.nguyen@leidos.com>
2019-08-21 12:18:38 +0100

    SGAR-1067 fix issue with unverified users
    Unverified users were able to bypass login process. Additional checks now in place to more accurately determine a logged in user.

--- src/common/middleware/usercheck.js
+++ src/common/middleware/usercheck.js
@@ -1,16 +1,24 @@
 const CookieModel = require('../../common/models/Cookie.class');
 const logger = require('../utils/logger')(__filename);
 
+const checkUserInCookie = (cookie) => {
+  const userDbIdNotSet = cookie.getUserDbId() === undefined || cookie.getUserDbId() === null;
+  const userVerifiedNotSet = cookie.getUserVerified() === undefined || cookie.getUserVerified() === null || !cookie.getUserVerified();
+  const userRoleNotSet = cookie.getUserRole() === undefined || cookie.getUserRole() === null;
+
+  return userDbIdNotSet || userVerifiedNotSet || userRoleNotSet;
+};
+
 module.exports = (req, res, next) => {
   const cookie = new CookieModel(req);
 
   // Checking for a blank referer appears to prevent a user from entering a URL in the browser
   // which may have been intended, but also seems a little off.
-  if (req.headers.referer === undefined || cookie.getUserDbId() === undefined || cookie.getUserDbId() === null) {
+  if (req.headers.referer === undefined || checkUserInCookie(cookie)) {
     if (req.headers.referer === undefined) {
       logger.info('About to redirect to login because the referer in headers is not set');
     }
-    if (cookie.getUserDbId() === undefined || cookie.getUserDbId() === null) {
+    if (checkUserInCookie(cookie)) {
       logger.info('About to redirect to login because the user is not set in the cookie');
     }
     res.redirect('/welcome/index');
--- src/test/user/login/get.controller.test.js
+++ src/test/user/login/get.controller.test.js
@@ -59,9 +59,53 @@ describe('User Login Get Controller', () => {
     expect(res.render).to.have.been.calledOnceWithExactly('app/user/login/index', { cookie });
   });
 
-  it('should redirect if there is a referrer and dbId set in the cookie', async () => {
+  it('should render if referer but no role in session', async () => {
     req.headers.referer = '/example';
-    req.session.u = { dbId: 'ALREADY_LOGGED_IN_ID' };
+    req.session.u = {
+      dbId: 'ALREADY_LOGGED_IN_ID',
+      vr: true,
+    };
+    const cookie = new CookieModel(req);
+    await controller(req, res);
+
+    expect(res.redirect).to.not.have.been.called;
+    expect(res.render).to.have.been.calledOnceWithExactly('app/user/login/index', { cookie });
+  });
+
+  it('should render if referer but no verified in session', async () => {
+    req.headers.referer = '/example';
+    req.session.u = {
+      dbId: 'ALREADY_LOGGED_IN_ID',
+      rl: 'User',
+    };
+    const cookie = new CookieModel(req);
+    await controller(req, res);
+
+    expect(res.redirect).to.not.have.been.called;
+    expect(res.render).to.have.been.calledOnceWithExactly('app/user/login/index', { cookie });
+  });
+
+  it('should render if referer but unverified in session', async () => {
+    req.headers.referer = '/example';
+    req.session.u = {
+      dbId: 'ALREADY_LOGGED_IN_ID',
+      vr: false,
+      rl: 'User',
+    };
+    const cookie = new CookieModel(req);
+    await controller(req, res);
+
+    expect(res.redirect).to.not.have.been.called;
+    expect(res.render).to.have.been.calledOnceWithExactly('app/user/login/index', { cookie });
+  });
+
+  it('should redirect if there is a referrer and dbId, role and verified set in the cookie', async () => {
+    req.headers.referer = '/example';
+    req.session.u = {
+      dbId: 'ALREADY_LOGGED_IN_ID',
+      rl: 'User',
+      vr: true,
+    };
 
     await controller(req, res);
 
