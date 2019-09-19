9a0db921498c973947b510ed15b49a3c6658be3b
Chun Chan <chun.chan@leidos.com>
2019-09-19 11:00:50 +0100

    Refactor to use eventlistener for onSubmit

--- src/public/javascripts/submit-button.js
+++ src/public/javascripts/submit-button.js
@@ -1,3 +1,7 @@
 function disableButton(id) {
   document.getElementById(id).setAttribute("disabled", "disabled");
 }
+
+function disableSubmitButton() {
+  document.getElementById('submit').setAttribute("disabled", "disabled");
+}
