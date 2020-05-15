29a33011e61f9b398f878c656c026822d4fcb769
Kamran Ali <kamran.ali@leidos.com>
2020-05-15 11:49:30 +0100

    unit test file cleanup

--- src/test/sanitise-value.test.js
+++ src/test/sanitise-value.test.js
@@ -1,109 +1,126 @@
 const { expect } = require('chai');
+
+require('./global.test');
+
 const sanitiseValue = require('../public/javascripts/sanitise-value');
 
-describe('Validator', () => {
-  // input day tests
-  it.only('Should remove the aphacharacters from string with input day', () => {
-    const testValue = '1a';
-    const type = 'day';
+describe('Sanitise value tests', () => {
+  let testValue;
+  let type;
+  let expectedResult;
+  let actualResult;
 
-    const expected = '1';
-    const actual = sanitiseValue(testValue, type);
 
-    expect(expected).to.equal(actual);
+  beforeEach(() => {
+    testValue = '';
+    type = '';
+
+    expectedResult = '';
+    actualResult = '';
   });
 
-  it.only('Should allow 2 digits for the day', () => {
-    const testValue = '234';
-    const type = 'day';
+  // input day tests
+  it('Should remove the aphacharacters from string with input day', () => {
+    testValue = '1a';
+    type = 'day';
 
-    const expected = '23';
-    const actual = sanitiseValue(testValue, type);
+    expectedResult = '1';
+    actualResult = sanitiseValue(testValue, type);
 
-    expect(expected).to.equal(actual);
+    expect(expectedResult).to.equal(actualResult);
+  });
+
+  it('Should allow 2 digits for the day', () => {
+    testValue = '234';
+    type = 'day';
+
+    expectedResult = '23';
+    actualResult = sanitiseValue(testValue, type);
+
+    expect(expectedResult).to.equal(actualResult);
   });
 
   // input month tests
-  it.only('Should remove the aphacharacters from string with input month', () => {
-    const testValue = '3a';
-    const type = 'month';
+  it('Should remove the aphacharacters from string with input month', () => {
+    testValue = '3a';
+    type = 'month';
 
-    const expected = '3';
-    const actual = sanitiseValue(testValue, type);
+    expectedResult = '3';
+    actualResult = sanitiseValue(testValue, type);
 
-    expect(expected).to.equal(actual);
+    expect(expectedResult).to.equal(actualResult);
   });
 
-  it.only('Should allow 2 digits for the day', () => {
-    const testValue = '200';
-    const type = 'month';
+  it('Should allow 2 digits for the day', () => {
+    testValue = '200';
+    type = 'month';
 
-    const expected = '20';
-    const actual = sanitiseValue(testValue, type);
+    expectedResult = '20';
+    actualResult = sanitiseValue(testValue, type);
 
-    expect(expected).to.equal(actual);
+    expect(expectedResult).to.equal(actualResult);
   });
 
   // input year tests
-  it.only('Should remove the aphacharacters from string with input year', () => {
-    const testValue = '2017aabbccddd';
-    const type = 'year';
+  it('Should remove the aphacharacters from string with input year', () => {
+    testValue = '2017aabbccddd';
+    type = 'year';
 
-    const expected = '2017';
-    const actual = sanitiseValue(testValue, type);
+    expectedResult = '2017';
+    actualResult = sanitiseValue(testValue, type);
 
-    expect(expected).to.equal(actual);
+    expect(expectedResult).to.equal(actualResult);
   });
 
-  it.only('Should allow 4 digits for the year', () => {
-    const testValue = '201689654';
-    const type = 'year';
+  it('Should allow 4 digits for the year', () => {
+    testValue = '201689654';
+    type = 'year';
 
-    const expected = '2016';
-    const actual = sanitiseValue(testValue, type);
+    expectedResult = '2016';
+    actualResult = sanitiseValue(testValue, type);
 
-    expect(expected).to.equal(actual);
+    expect(expectedResult).to.equal(actualResult);
   });
 
   // input hour tests
-  it.only('Should remove the aphacharacters from string with input hour', () => {
-    const testValue = '15gregr';
-    const type = 'hour';
+  it('Should remove the aphacharacters from string with input hour', () => {
+    testValue = '15gregr';
+    type = 'hour';
 
-    const expected = '15';
-    const actual = sanitiseValue(testValue, type);
+    expectedResult = '15';
+    actualResult = sanitiseValue(testValue, type);
 
-    expect(expected).to.equal(actual);
+    expect(expectedResult).to.equal(actualResult);
   });
 
-  it.only('Should allow 2 digits for the hour', () => {
-    const testValue = '4615';
-    const type = 'hour';
+  it('Should allow 2 digits for the hour', () => {
+    testValue = '4615';
+    type = 'hour';
 
-    const expected = '46';
-    const actual = sanitiseValue(testValue, type);
+    expectedResult = '46';
+    actualResult = sanitiseValue(testValue, type);
 
-    expect(expected).to.equal(actual);
+    expect(expectedResult).to.equal(actualResult);
   });
 
   // input minute tests
-  it.only('Should remove the aphacharacters from string with input minute', () => {
-    const testValue = '42gregr';
-    const type = 'minute';
+  it('Should remove the aphacharacters from string with input minute', () => {
+    testValue = '42gregr';
+    type = 'minute';
 
-    const expected = '42';
-    const actual = sanitiseValue(testValue, type);
+    expectedResult = '42';
+    actualResult = sanitiseValue(testValue, type);
 
-    expect(expected).to.equal(actual);
+    expect(expectedResult).to.equal(actualResult);
   });
 
-  it.only('Should allow 2 digits for the minute', () => {
-    const testValue = '52148';
-    const type = 'minute';
+  it('Should allow 2 digits for the minute', () => {
+    testValue = '52148';
+    type = 'minute';
 
-    const expected = '52';
-    const actual = sanitiseValue(testValue, type);
+    expectedResult = '52';
+    actualResult = sanitiseValue(testValue, type);
 
-    expect(expected).to.equal(actual);
+    expect(expectedResult).to.equal(actualResult);
   });
 });
