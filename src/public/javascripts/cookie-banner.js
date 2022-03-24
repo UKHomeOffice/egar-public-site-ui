10a6fd3bd7bd7fa46167acc3f74bf7eabbbc11c8
Chris Hutchings <christopher.hutchings-cic-uk@ibm.com>
2022-03-24 18:31:41 +0000

    Updated packages, implemented cookie compliance and search/sort on people

--- src/public/javascripts/cookie-banner.js
+++ src/public/javascripts/cookie-banner.js
@@ -5,16 +5,94 @@ function setCookie(name, value, expiryDays) {
   document.cookie = name + "=" + value + ";" + expires + ";path=/";
 };
 
+var CONSENT_COOKIE_NAME = 'sgar_cookies_policy';
+var DEFAULT_COOKIE_CONSENT = 'false';
+
+
 function hasSeenCookie(cookieName) {
   return document.cookie.includes(cookieName);
 };
 
 function showCookieBanner(){
-  const banner = document.getElementsByClassName("app-cookie-banner")[0];
+  const banner = document.getElementsByClassName("govuk-cookie-banner")[0];
   banner.style.display = "block";
+
 };
 
-if (!hasSeenCookie('seen_cookie_message')) {
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function deleteCookie (name) {
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/';
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;domain=' + window.location.hostname + ';path=/';
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT;domain=.' + window.location.hostname + ';path=/';
+}
+
+function Cookie (name, value) {
+  if (typeof value !== 'undefined') {
+    if (value === false || value === null) {
+      deleteCookie(name);
+    }
+  else {
+    return getCookie(name)
+      }
+  }
+}
+
+//if (!hasSeenCookie('seen_cookie_message')) {
+// showCookieBanner();
+//  setCookie('seen_cookie_message', 'yes', 30);
+//};
+
+if (getCookie('sgar_cookies_policy') === '' || getCookie('sgar_cookies_policy') === null){
   showCookieBanner();
-  setCookie('seen_cookie_message', 'yes', 30);
-};
+}
+
+function accept_cookies(){
+  document.getElementById("cookies_accept_message").style.display = "block";
+  document.getElementById("main_cookie_message").style.display = "none";
+  setCookie('sgar_cookies_policy', true, 30);
+}
+
+function reject_cookies(){
+  document.getElementById("cookies_reject_message").style.display = "block";
+  document.getElementById("main_cookie_message").style.display = "none";
+  setCookie('sgar_cookies_policy', false, 30);
+}
+
+function hide_message(){
+  document.getElementById("main_cookie_banner").style.display = "none";
+}
+
+function change_cookie_preference(){
+  if (document.getElementById('radio_cookies_accept').checked == true){
+    setCookie('sgar_cookies_policy', true, 30);
+    document.getElementById("preference_changed").removeAttribute('hidden');
+    document.getElementById("preference_changed").focus();
+  }
+  else if (document.getElementById('radio_cookies_reject').checked == true){
+    setCookie('sgar_cookies_policy', false, 30);
+    document.getElementById("preference_changed").removeAttribute('hidden');
+    document.getElementById("preference_changed").focus();
+    deleteCookie('_ga');
+    deleteCookie('_gid');
+    deleteCookie('_gat_gtag_UA + {{ ga_id }}');
+    deleteCookie('_gat');
+
+  }
+  
+  else{
+    setCookie('sgar_cookies_policy', false, 30);
+  }
+}
+
+
+
