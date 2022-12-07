f61e9477603ccfd7754c984a1d518d937d638e73
Christopher Hutchings <34134325+christopher-hutchings@users.noreply.github.com>
2022-12-07 17:10:23 +0000

    downgraded packages (#280)

--- src/app/garfile/arrival/post.controller.js
+++ src/app/garfile/arrival/post.controller.js
@@ -86,7 +86,7 @@ const buildValidations = (voyage) => {
   const validations = [
     [new ValidationRule(validator.realDate, 'arrivalDate', arriveDateObj, realDateMsg)],
     [new ValidationRule(validator.currentOrFutureDate, 'arrivalDate', arriveDateObj, futureDateMsg)],
-    // [new ValidationRule(validator.dateTooFarInFuture, 'arrivalDate', arriveDateObj, __('field_arrival_date_too_far_in_future'))],
+    [new ValidationRule(validator.dateTooFarInFuture, 'arrivalDate', arriveDateObj, __('field_arrival_date_too_far_in_future'))],
     [new ValidationRule(validator.validTime, 'arrivalTime', arrivalTimeObj, timeMsg)],
     [new ValidationRule(validator.notEmpty, 'portChoice', voyage.portChoice, portChoiceMsg)],
   ];
