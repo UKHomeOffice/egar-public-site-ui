e145c81b785bf4a3bfa77e5da8a4ab9794d9ba2c
Christopher Hutchings <34134325+christopher-hutchings@users.noreply.github.com>
2022-12-08 16:13:48 +0000

    Updated customs validation (#284)

--- src/app/garfile/review/validations.js
+++ src/app/garfile/review/validations.js
@@ -7,12 +7,13 @@ const validator = require('../../../common/utils/validator');
 const validateCaptainCrewMsg = 'There must be at least one captain or crew member on the voyage.';
 const registrationMsg = 'Aircraft registration must be completed';
 const responsibleMsg = 'Responsible person details must be completed';
+const customsMsg = 'Customs Declaration question not answered';
 
 
 module.exports.validations = (garfile, garpeople) => {
   const voyageDateMsg = i18n.__('validator_msg_voyage_dates');
   const {
-    departureDate, departureTime, arrivalDate, arrivalTime, registration, responsibleGivenName, prohibitedGoods, baggage, visitReason,
+    departureDate, departureTime, arrivalDate, arrivalTime, registration, responsibleGivenName, prohibitedGoods, baggage, visitReason, intentionValue,
   } = garfile;
   const voyageDateTimeObj = {
     departureDate, departureTime, arrivalDate, arrivalTime,
@@ -34,6 +35,9 @@ module.exports.validations = (garfile, garpeople) => {
     // Customs screen validations
     [
       new ValidationRule(validator.notEmpty, 'customs', visitReason, 'Visit Reason question not answered'),
+    ],
+    [
+      new ValidationRule(validator.notEmpty, 'intentionValue', intentionValue, customsMsg),
     ],
   ];
 };
