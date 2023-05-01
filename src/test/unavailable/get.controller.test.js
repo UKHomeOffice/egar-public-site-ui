36950124fbbe0cc947ae45d185bc7f5d7ac45d45
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2023-05-01 14:36:31 +0100

    fixed unit tests

--- src/test/unavailable/get.controller.test.js
+++ src/test/unavailable/get.controller.test.js
@@ -4,14 +4,11 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
+const controller = require('../../app/unavailable/get.controller');
 const availability = require('../../common/config/availability');
-
-availability.ENABLE_UNAVAILABLE_PAGE = 'TRUE';
 
 require('../global.test');
 const CookieModel = require('../../common/models/Cookie.class');
-
-const controller = require('../../app/unavailable/get.controller');
 
 describe('User Login Get Controller', () => {
   let req; let res;
@@ -34,6 +31,8 @@ describe('User Login Get Controller', () => {
 
   it('should render the appropriate page', async () => {
     const cookie = new CookieModel(req);
+    sinon.stub(availability, 'ENABLE_UNAVAILABLE_PAGE').value('TRUE');
+
     await controller(req, res);
 
     expect(res.render).to.have.been.calledWith('app/unavailable/index', { cookie });
