2cf4369bc21e95a0247a589ec9db8ba357a6490a
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2023-07-11 19:14:01 +0100

    added jQuery polling to GAR AMG page

--- src/app/garfile/amg/get.controller.js
+++ src/app/garfile/amg/get.controller.js
@@ -2,14 +2,13 @@ const logger = require('../../../common/utils/logger')(__filename);
 const CookieModel = require('../../../common/models/Cookie.class');
 const manifestFields = require('../../../common/seeddata/gar_manifest_fields.json');
 const garApi = require('../../../common/services/garApi');
-//const validator = require('../../../common/utils/validator');
-//const validationList = require('./validations');
 
 module.exports = (req, res) => {
   logger.debug('In garfile / amg get controller');
 
   const cookie = new CookieModel(req);
   const garId = cookie.getGarId();
+  const template = req.query.template==='pane' ? 'app/garfile/amg/pane' : 'app/garfile/amg/index';
 
   Promise.all([
     garApi.get(garId),
@@ -17,17 +16,9 @@ module.exports = (req, res) => {
     garApi.getSupportingDocs(garId),
   ]).then((apiResponse) => {
     const garfile = JSON.parse(apiResponse[0]);
-   // validator.handleResponseError(garfile);
-
     const garpeople = JSON.parse(apiResponse[1]);
-    //validator.handleResponseError(garpeople);
-
-    console.error(garpeople);
-
     const garsupportingdocs = JSON.parse(apiResponse[2]);
-    //validator.handleResponseError(garsupportingdocs);
 
-    //const validations = validationList.validations(garfile, garpeople);
     const renderObj = {
       cookie,
       manifestFields,
@@ -37,7 +28,8 @@ module.exports = (req, res) => {
       showChangeLinks: true,
     };
 
-    res.render('app/garfile/amg/index', renderObj);
+    res.render(template, renderObj);
+
   }).catch((err) => {
     logger.error('Error retrieving GAR for amg');
     logger.error(JSON.stringify(err));
