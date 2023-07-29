d51b231b271f2b0101aef05895c5b62e2894a15f
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2023-07-29 22:03:40 +0100

    added passenger confirmation endpoint

--- src/common/config/endpoints.js
+++ src/common/config/endpoints.js
@@ -125,6 +125,11 @@ const endpoints = {
     logger.debug(`Calling get GAR endpoint ${endpoint}`);
     return endpoint;
   },
+  postGarPassengerConfirmations(garId){
+    const endpoint = new URL(`${API_VERSION}/gar/${garId}/confirmation`, BASE_URL).href;
+    logger.debug(`Calling post GAR passenger confirmations endpoint ${endpoint}`);
+    return endpoint;
+  },
   postFile(garId) {
     const endpoint = new URL(`${API_VERSION}/gar/${garId}`, BASE_URL).href;
     logger.debug(`Calling get GAR endpoint ${endpoint}`);
