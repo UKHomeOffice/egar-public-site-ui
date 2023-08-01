1a11f2d5cc1485d302af7e78ca5e2a991008485a
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2023-08-01 14:39:49 +0100

    uncommented usercheck / referer check

--- src/common/middleware/usercheck.js
+++ src/common/middleware/usercheck.js
@@ -14,8 +14,7 @@ module.exports = (req, res, next) => {
 
   // Checking for a blank referer appears to prevent a user from entering a URL in the browser
   // which may have been intended, but also seems a little off.
-  //if (req.headers.referer === undefined || checkUserInCookie(cookie)) {
-  if (false) {
+  if (req.headers.referer === undefined || checkUserInCookie(cookie)) {
     if (req.headers.referer === undefined) {
       logger.info('About to redirect to login because the referer in headers is not set');
     }
