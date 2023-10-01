15738bd995cfa3cd0e5593306175b03700aa89c3
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2023-10-01 14:02:09 +0100

    fixed unit test: should go to craft page if buttonClicked property is set

--- src/test/garfile/customs/post.controller.test.js
+++ src/test/garfile/customs/post.controller.test.js
@@ -333,6 +333,8 @@ describe('GAR Customs Post Controller', () => {
       expect(garApiPatchStub).to.have.been.calledWith('ABCD-1234', 'Draft', {
         baggage: undefined,
         baggageDeclaration:'',
+        continentalShelf: 'No',
+        continentalShelfDeclaration: '',
         freeCirculation: 0,
         goodsDeclaration: "Duty Free",
         intentionValue: "Yes",
