82439c0a07351bcec184f1d1af6148416726e7c8
Alastair <alastair.mottram-epson@digital.homeoffice.gov.uk>
2023-10-09 15:05:37 +0100

    sGAR ~ 2023.10.09 ~ gar ahs been cancelled, for the all stuff.

--- src/app/garfile/cancel/post.controller.js
+++ src/app/garfile/cancel/post.controller.js
@@ -7,28 +7,54 @@ const config = require('../../../common/config');
 module.exports = (req, res) => {
   logger.debug('In garfile / cancel get controller');
   const cookie = new CookieModel(req);
-  garApi
-    .patch(cookie.getGarId(), 'Cancelled', {})
+
+  garApi.getPeople(cookie.getGarId())
+    .then((res) => {
+      const garpeople = JSON.parse(res).items;
+      const VALID_PERMISSION_TO_TRAVEL = "0A";
+
+      const passengersToRaiseException = garpeople
+        .filter(garperson => garperson.amgCheckinResponseCode === VALID_PERMISSION_TO_TRAVEL)
+        .map(garperson => garperson.garPeopleId);
+
+      return passengersToRaiseException;
+    })
+    .then((passengersToRaiseException) => {
+      return garApi.postGarPassengerConfirmations(cookie.getGarId(), passengersToRaiseException)
     .then(() => {
-      emailService.send(config.NOTIFY_GAR_CANCEL_TEMPLATE_ID, cookie.getUserEmail(), {
-        firstName: cookie.getUserFirstName(),
-        garId: cookie.getGarId(),
-      }).then(() => {
-        req.session.successMsg = 'The GAR has been successfully cancelled';
-        req.session.successHeader = 'Cancellation Confirmation';
-        req.session.save(() => {
-          res.redirect('/home');
-        });
-      }).catch(() => {
-        req.session.successMsg = 'The GAR has been successfully cancelled, but there was a problem with sending the email';
-        req.session.successHeader = 'Cancellation Confirmation';
-        req.session.save(() => {
-          res.redirect('/home');
-        });
+      garApi.patch(cookie.getGarId(), 'Cancelled', {})
+        .then(() => {
+          emailService.send(config.NOTIFY_GAR_CANCEL_TEMPLATE_ID, cookie.getUserEmail(), {
+            firstName: cookie.getUserFirstName(),
+            garId: cookie.getGarId(),
+          }).then(() => {
+            req.session.successMsg = 'The GAR has been successfully cancelled';
+            req.session.successHeader = 'Cancellation Confirmation';
+            req.session.save(() => {
+              res.redirect('/home');
+            });
+          }).catch(() => {
+            req.session.successMsg = 'The GAR has been successfully cancelled, but there was a problem with sending the email';
+            req.session.successHeader = 'Cancellation Confirmation';
+            req.session.save(() => {
+              res.redirect('/home');
+            });
+          });
+        })
+      .catch((err) => {
+        logger.error(err);
+        res.render('app/garfile/cancel', { cookie, error: [{ message: 'Failed to cancel GAR' }] });
       });
     })
     .catch((err) => {
-      logger.error(err);
-      res.render('app/garfile/cancel', { cookie, error: [{ message: 'Failed to cancel GAR' }] });
+        logger.error('Api failed to post GAR exceptions');
+        logger.error(err);
+        res.render('app/home', {
+            cookie,
+            errors: [{
+                message: 'Failed to post GAR confirmation|exceptions',
+            }],
+        });
+      });
     });
 };
