ff72a5f233f7fcf9f473021ebc0bff417c2dc570
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2023-10-11 17:14:39 +0100

    removed amg/departure, amg/departurestatus, removed code that linked to those pages, removed from router

--- src/app/garfile/amg/departure/index.js
+++
@@ -1,5 +0,0 @@
-const getController = require('./get.controller');
-const postController = require('./post.controller');
-const nav = require('../../../../common/utils/nav');
-
-module.exports = nav.buildRouterAndPaths('/garfile/amg/departure', getController, postController);
--- src/app/garfile/amg/departurestatus/index.js
+++
@@ -1,4 +0,0 @@
-const getController = require('./get.controller');
-const nav = require('../../../../common/utils/nav');
-
-module.exports = nav.buildRouterAndPaths('/garfile/amg/departurestatus', getController);
