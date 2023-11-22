f12f5588e8ac41b542db335a5a604b1ec19b09c4
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2023-11-22 19:11:04 +0000

    updates based on sGAR biz team feedback

--- src/app/help/get.controller.js
+++ src/app/help/get.controller.js
@@ -3,11 +3,6 @@ const logger = require('../../common/utils/logger')(__filename);
 module.exports = (req, res) => {
   logger.debug('In help get controller');
 
-  if(req.query.tab && req.query.tab.toUpperCase() === 'UPT' ){
-    res.render('app/help/upt');
-  }
-  else{
-    res.render('app/help/index');
-  }
-  
+  res.render('app/help/index');
+
 };
