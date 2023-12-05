5f7c396c0cfd6a8c6e255157bc23245d1c36c423
Alastair <alastair.mottram-epson@digital.homeoffice.gov.uk>
2023-12-05 11:14:16 +0000

    sGAR ~ 2023.12.05 ~ kind of fix for the manger edit problem, maybe an issue with the code or not.

--- src/app/organisation/get.controller.js
+++ src/app/organisation/get.controller.js
@@ -3,16 +3,30 @@ const CookieModel = require('../../common/models/Cookie.class');
 const orgApi = require('../../common/services/organisationApi');
 const logger = require('../../common/utils/logger')(__filename);
 
+permissionLevels = {
+  'Individual': 0, 
+  'User': 0, 
+  'Manager': 1, 
+  'Admin': 2
+}
+
 module.exports = (req, res) => {
   logger.debug('In organisation get controller');
   const cookie = new CookieModel(req);
   const errMSg = { message: 'Failed to get orgusers' };
-  
+  const userPermissions = permissionLevels[cookie.getUserRole()]
 
   orgApi.getUsers(cookie.getOrganisationId())
     .then((values) => {
-      const orgUsers = JSON.parse(values);
+      //TODO if no items --> analyse this
+      const orgUsers = JSON.parse(values).items.map((orgUser) => {
+        const isEditable = userPermissions > permissionLevels[orgUser.role.name];
+        return { ...orgUser, isEditable };
+      });
       cookie.setOrganisationUsers(orgUsers);
+
+      logger.info(`cookie email: ${cookie.getUserEmail()}`);
+
       if (req.session.errMsg) {
         const { errMsg } = req.session;
         delete req.session.errMsg;
