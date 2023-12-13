6d6f71c1fe330c878ae67e2fe53df5fec01c573a
Sunish Jose <>
2023-12-13 14:45:54 +0000

    FAIR-304 Tests

---
+++ src/test/accessibility/get.controller.test.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+
+const sinon = require('sinon');
+const { expect } = require('chai');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+
+require('../global.test');
+
+const controller = require('../../app/accessibility/get.controller');
+
+describe('Accessibility Get Controller', () => {
+  let req; let res;
+
+  beforeEach(() => {
+    chai.use(sinonChai);
+
+    req = {
+      session: {},
+    };
+    res = {
+      render: sinon.spy(),
+    };
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should render the appropriate page', async () => {
+    await controller(req, res);
+
+    expect(res.render).to.have.been.calledWith('app/accessibility/index');
+  });
+});
