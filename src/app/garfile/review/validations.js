74855b5adf7ac40fcf2067255099bcbd3c7ed491
Sunish Jose <>
2023-12-14 11:02:34 +0000

    FAIR-321 (Validate the special characters in the existing passenger names)

--- src/app/garfile/review/validations.js
+++ src/app/garfile/review/validations.js
@@ -18,26 +18,39 @@ module.exports.validations = (garfile, garpeople) => {
   const voyageDateTimeObj = {
     departureDate, departureTime, arrivalDate, arrivalTime,
   };
+  const validationArr = [];
+  garpeople.items.forEach((crew) => {
+    const crewLabel = i18n.__('validator_api_uploadgar_people_type_crew');
+    const passengerLabel = i18n.__('validator_api_uploadgar_people_type_passenger');
+    const peopleType = crew.peopleType === 'Crew' ? crewLabel : passengerLabel;
+    const name = i18n.__('validator_api_uploadgar_person_type_person_name', { peopleType, firstName: crew.firstName, lastName: crew.lastName });
+    validationArr.push([
+      new ValidationRule(validator.isAlpha, '', crew.lastName, `Surname for ${name} must not contain special characters or numbers, 
+      you may also remove/update the special characters by going to Edit a person page.`),
+    ]);
+    validationArr.push([
+      new ValidationRule(validator.isAlpha, '', crew.firstName, `Given name for ${name} must not contain special characters or numbers,
+      you may also remove/update the special characters by going to Edit a person page.`),
+    ]);
+  });
 
-  return [
-    [
-      new ValidationRule(validator.isValidDepAndArrDate, 'departure', voyageDateTimeObj, voyageDateMsg),
-    ],
-    [
-      new ValidationRule(validator.notEmpty, 'aircraft', registration, registrationMsg),
-    ],
-    [
-      new ValidationRule(validator.notEmpty, 'manifest', garpeople.items, validateCaptainCrewMsg),
-    ],
-    [
-      new ValidationRule(validator.notEmpty, 'responsiblePerson', responsibleGivenName, responsibleMsg),
-    ],
-    // Customs screen validations
-    [
-      new ValidationRule(validator.notEmpty, 'customs', visitReason, 'Visit Reason question not answered'),
-    ],
-    [
-      new ValidationRule(validator.notEmpty, 'intentionValue', intentionValue, customsMsg),
-    ],
-  ];
+  validationArr.push([
+    new ValidationRule(validator.isValidDepAndArrDate, 'departure', voyageDateTimeObj, voyageDateMsg),
+  ]);
+  validationArr.push([
+    new ValidationRule(validator.notEmpty, 'aircraft', registration, registrationMsg),
+  ]);
+  validationArr.push([
+    new ValidationRule(validator.notEmpty, 'manifest', garpeople.items, validateCaptainCrewMsg),
+  ]);
+  validationArr.push([
+    new ValidationRule(validator.notEmpty, 'responsiblePerson', responsibleGivenName, responsibleMsg),
+  ]);
+  validationArr.push([
+    new ValidationRule(validator.notEmpty, 'customs', visitReason, 'Visit Reason question not answered'),
+  ]);
+  validationArr.push([
+    new ValidationRule(validator.notEmpty, 'intentionValue', intentionValue, customsMsg),
+  ]);
+  return validationArr;
 };
