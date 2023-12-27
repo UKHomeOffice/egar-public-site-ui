34fe1eb968f7256c3d020a3035bbf53c5922aa8c
Alastair <alastair.mottram-epson@digital.homeoffice.gov.uk>
2023-12-27 12:58:54 +0000

    sGAR ~ 2023.12.27 ~ menu is now dynamic, so that user db if can occur.

--- src/app/help/get.controller.js
+++ src/app/help/get.controller.js
@@ -1,8 +1,12 @@
 const logger = require('../../common/utils/logger')(__filename);
+const CookieModel = require('../../common/models/Cookie.class');
 
+
+// cookie model, use that in the njks
 module.exports = (req, res) => {
+  const cookie = new CookieModel(req);
   logger.debug('In help get controller');
 
-  res.render('app/help/index');
+  res.render('app/help/index', { cookie });
 
 };
