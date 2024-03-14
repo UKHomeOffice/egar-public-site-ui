fee8812994b64fdaab47b17c4653c3d6b7a79f4f
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2024-03-14 20:15:38 +0000

    added new ui files

---
+++ src/app/garfile/amend/post.controller.js
@@ -0,0 +1,67 @@
+const logger = require('../../../common/utils/logger')(__filename);
+const CookieModel = require('../../../common/models/Cookie.class');
+const garApi = require('../../../common/services/garApi');
+const emailService = require('../../../common/services/sendEmail');
+const config = require('../../../common/config');
+
+module.exports = (req, res) => {
+  logger.debug('In garfile / amend post controller');
+  const cookie = new CookieModel(req);
+  const isCancelled = true
+
+  garApi.getPeople(cookie.getGarId())
+    .then((res) => {
+      const garpeople = JSON.parse(res).items;
+      const passengersToRaiseException = garpeople
+        .filter(garperson => garperson.amgCheckinResponseCode !== null)
+        .map(garperson => garperson.garPeopleId);
+
+      return passengersToRaiseException;
+    })
+    .then((passengersToRaiseException) => {
+      if (passengersToRaiseException.length === 0) {
+        return null
+      } else {
+        garApi.postGarPassengerConfirmations(
+          cookie.getGarId(), 
+          passengersToRaiseException,
+          isCancelled
+        );
+      }
+    })
+    .then(() => {
+      garApi.patch(cookie.getGarId(), 'Draft', {})
+        .then(() => {
+          emailService.send(config.NOTIFY_GAR_CANCEL_TEMPLATE_ID, cookie.getUserEmail(), {
+            firstName: cookie.getUserFirstName(),
+            garId: cookie.getGarId(),
+          }).then(() => {
+            req.session.successMsg = 'The GAR has been successfully cancelled, you may now amend it and resubmit it.';
+            req.session.successHeader = 'Cancellation Confirmation';
+            req.session.save(() => {
+              res.redirect('/home');
+            });
+          }).catch(() => {
+            req.session.successMsg = 'The GAR has been successfully cancelled, but there was a problem with sending the email. You may now amend it and resubmit it.';
+            req.session.successHeader = 'Cancellation Confirmation';
+            req.session.save(() => {
+              res.redirect('/home');
+            });
+          });
+        })
+      .catch((err) => {
+        logger.error(err);
+        res.render('app/garfile/amend', { cookie, error: [{ message: 'Failed to cancel GAR' }] });
+      });
+    })
+    .catch((err) => {
+      logger.error('Api failed to post GAR exceptions');
+      logger.error(err);
+      res.render('app/home', {
+          cookie,
+          errors: [{
+              message: 'Failed to post GAR confirmation|exceptions',
+          }],
+      });
+    });
+};
