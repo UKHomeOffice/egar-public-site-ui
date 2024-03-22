83302391aece5d8d2a76f4832b15c78fcdd0fd38
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2024-03-22 13:34:52 +0000

    updated logging to include amend user and gar Ids. removed unused includes.

--- src/app/garfile/amend/post.controller.js
+++ src/app/garfile/amend/post.controller.js
@@ -1,12 +1,10 @@
 const logger = require('../../../common/utils/logger')(__filename);
 const CookieModel = require('../../../common/models/Cookie.class');
 const garApi = require('../../../common/services/garApi');
-const emailService = require('../../../common/services/sendEmail');
-const config = require('../../../common/config');
 
 module.exports = (req, res) => {
-  logger.debug('In garfile / amend post controller');
   const cookie = new CookieModel(req);
+  logger.debug(`In garfile / amend post controller - User: ${cookie.getUserDbId()} editing GAR: ${cookie.getGarId()}`);
 
   garApi.patch(cookie.getGarId(), 'Draft', {})
     .then(() => {
