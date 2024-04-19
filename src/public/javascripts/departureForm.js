196a2fa68a9463dcc053c6716e3b70f1c576e67c
Alastair <alastair.mottram-epson@digital.homeoffice.gov.uk>
2024-04-19 10:16:56 +0100

    sGAR ~ 2024.04.19 ~ add back in the dialog changes.

--- src/public/javascripts/departureForm.js
+++ src/public/javascripts/departureForm.js
@@ -13,27 +13,110 @@ const departureLongDegrees = document.getElementById("departureLongDegrees");
 const departureLongMinutes = document.getElementById("departureLongMinutes");
 const departureLongSeconds = document.getElementById("departureLongSeconds");
 
+const pageForm = document.getElementById('page-form');
+const confirmWarnedDepartureDialog = document.getElementById("confirmWarnedDepartureDialog")
+const continueWithWarnedDate = document.getElementById('continueWithWarnedDate');
+
+const twoHourWarningTexts = Array.from(document.getElementsByClassName("twoHourWarningText"));
+const fortyEightHourWarningTexts = Array.from(document.getElementsByClassName("fortyEightHourWarningText"));
+
+let departureFormSubmitter = undefined;
+
+dialogPolyfill.registerDialog(confirmWarnedDepartureDialog);
+
+const departureDate =  () => {
+  return new Date(
+    Number(departureYear.value),
+    Number(departureMonth.value) - 1,
+    Number(departureDay.value),
+    Number(departureHourTime.value),
+    Number(departureMinuteTime.value)
+  )
+};
+  
+
+function showDepartureDateWarningMessages(providedDate) {
+  console.log(providedDate);
+  twoHourWarningTexts.forEach($element => $element.hidden = isTwoHoursPriorDeparture(providedDate));
+  fortyEightHourWarningTexts.forEach($element => $element.hidden = dateNotMoreThanTwoDaysInFuture(providedDate));
+}
+
+window.addEventListener("load", () => {
+    // So it does not show warning message when form is blank
+    if ([
+      departureYear.value, 
+      departureMonth.value,
+      departureDay.value,
+      departureHourTime.value,
+      departureMinuteTime.value
+    ].includes('')) {
+      return;
+    }
+
+  showDepartureDateWarningMessages(departureDate());
+})
+
+
+pageForm.addEventListener("submit", (e) => {
+    // Instead of dialog confirmination, it will submit the form so post controller validations catches this.
+  if ([
+    departureYear.value, 
+    departureMonth.value,
+    departureDay.value,
+    departureHourTime.value,
+    departureMinuteTime.value
+  ].includes('')) {
+    return;
+  }
+
+  if (departureFormSubmitter) {
+    return;
+  }
+  
+  if (isTwoHoursPriorDeparture(departureDate()) && dateNotMoreThanTwoDaysInFuture(departureDate())) {
+    return;
+  }
+
+  e.preventDefault();
+  departureFormSubmitter = e.submitter;
+  showDepartureDateWarningMessages(departureDate());
+  confirmWarnedDepartureDialog.showModal();
+  
+});
+
+confirmWarnedDepartureDialog.addEventListener("close", (e) => {
+  departureFormSubmitter = undefined;
+});
+continueWithWarnedDate.addEventListener("click", (e) => {
+    pageForm.requestSubmit(departureFormSubmitter);
+});
+
 departureDay.addEventListener("keyup", (e) => {
   e.target.value = sanitiseDateOrTime(e.target.value, 'day');
   autoTab(departureDay, 'day', departureMonth);
+  showDepartureDateWarningMessages(departureDate());
 });
 
 departureMonth.addEventListener("keyup", (e) => {
   e.target.value = sanitiseDateOrTime(e.target.value, 'month');
   autoTab(departureMonth, 'month', departureYear);
+  showDepartureDateWarningMessages(departureDate());
 });
 
 departureYear.addEventListener("keyup", (e) => {
   e.target.value = sanitiseDateOrTime(e.target.value, 'year')
+  showDepartureDateWarningMessages(departureDate());
 });  
 
 departureHourTime.addEventListener("keyup", (e) => {
   e.target.value = sanitiseDateOrTime(e.target.value, 'hour');
   autoTab(departureHourTime, 'hour', departureMinuteTime);
+  showDepartureDateWarningMessages(departureDate());
 });
 
 departureMinuteTime.addEventListener("keyup", (e) => {
   e.target.value = sanitiseDateOrTime(e.target.value, 'minute');
+  showDepartureDateWarningMessages(departureDate());
 });
 
 departureDegrees.addEventListener("keyup", (e) => {
