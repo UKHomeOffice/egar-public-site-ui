b91be1ce45ca43b7041f1e802c57ffd615226911
Justin Hylton HO <132352825+justinhyltonHO@users.noreply.github.com>
2024-05-14 19:45:47 +0100

    Added postcode validation

--- src/app/garfile/responsibleperson/validations.js
+++ src/app/garfile/responsibleperson/validations.js
@@ -61,6 +61,7 @@ module.exports.validations = (req) => {
       new ValidationRule(validator.isAddressValidCharacters, 'responsibleTown', responsibleTown, `Town or city must not contain special characters such as $ % ' or ä`),
     ],
     [
+      new ValidationRule(validator.notEmpty, 'responsiblePostcode', responsiblePostcode, 'Enter a postcode for the responsible person.'),
       new ValidationRule(validator.validTextLength, 'responsiblePostcode', { value: responsiblePostcode, maxLength: MAX_POSTCODE_LENGTH }, `Postcode must be ${MAX_POSTCODE_LENGTH} characters or less`),
       new ValidationRule(validator.isPostCodeValidCharacters, 'responsiblePostcode', responsiblePostcode, `Postcode must not contain special characters such as $ % ' or ä`),
     ],
