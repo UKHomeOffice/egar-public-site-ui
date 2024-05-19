0b209ff7376cd04f5df28ca482d783a9992afc74
Alastair <alastair.mottram-epson@digital.homeoffice.gov.uk>
2024-05-19 17:36:39 +0100

    sGAR ~ 2024.05.19 ~ other validations are woird.

--- src/app/people/validations.js
+++ src/app/people/validations.js
@@ -7,53 +7,7 @@ module.exports.validations = (req) => {
   const expiryDateObj = { d: req.body.expiryDay, m: req.body.expiryMonth, y: req.body.expiryYear };
   const docTypeOther = req.body.travelDocumentType;
 
-  if (docTypeOther === 'Other') {
-    return [
-      [
-        new ValidationRule(validator.bornAfter1900, 'dob', dobObj, 'Enter a real date of birth'),
-      ],
-      [
-        new ValidationRule(validator.realDateInFuture, 'documentExpiryDate', expiryDateObj, 'Enter a real document expiry date'),
-      ],
-      [
-        new ValidationRule(validator.notEmpty, 'firstName', req.body.firstName, 'Enter the given name of the person'),
-        new ValidationRule(validator.isValidStringLength, 'firstName', req.body.firstName, `Given name must be ${MAX_STRING_LENGTH} characters or less`),
-      ],
-      [
-        new ValidationRule(validator.notEmpty, 'lastName', req.body.lastName, 'Enter the surname of the person'),
-        new ValidationRule(validator.isValidStringLength, 'lastName', req.body.lastName, `Surname must be ${MAX_STRING_LENGTH} characters or less`),
-      ],
-      [
-        new ValidationRule(validator.notEmpty, 'nationality', req.body.nationality, 'Enter the nationality of the person'),
-        new ValidationRule(validator.validISO3Country, 'nationality', req.body.nationality, 'Enter a valid nationality for the person, it must be a ISO 3166 country code.'),
-      ],
-      [
-        new ValidationRule(validator.isValidOptionalStringLength, 'birthplace', req.body.birthplace, `Place of birth must be ${MAX_STRING_LENGTH} characters or less`),
-      ],
-      [
-        new ValidationRule(validator.notEmpty, 'personType', req.body.personType, 'Select the type of person'),
-      ],
-      [
-        new ValidationRule(validator.notEmpty, 'gender', req.body.gender, 'Select the sex of the person'),
-      ],
-      [
-        new ValidationRule(validator.notEmpty, 'travelDocumentNumber', req.body.travelDocumentNumber, 'Enter the travel document number'),
-        new ValidationRule(validator.isValidStringLength, 'travelDocumentNumber', req.body.travelDocumentNumber, `Travel document number must be ${MAX_STRING_LENGTH} characters or less`),
-        new ValidationRule(validator.isAlphanumeric, 'travelDocumentNumber', req.body.travelDocumentNumber, `Travel document number must be alphanumeric only.`),
-      ],
-      [
-        new ValidationRule(validator.notEmpty, 'travelDocumentType', req.body.travelDocumentType, 'Select the travel document type'),
-      ],
-      [
-        new ValidationRule(validator.notEmpty, 'issuingState', req.body.issuingState, 'Enter the issuing state for the document'),
-      ],
-      [
-        new ValidationRule(validator.notEmpty, 'travelDocumentOther', req.body.travelDocumentOther, 'Enter the document type you are using'),
-        new ValidationRule(validator.validName, 'travelDocumentOther', req.body.travelDocumentOther, 'Enter a real document type'),
-      ],
-    ];
-  }
-  return [
+  const peopleValidationRules = [
     [
       new ValidationRule(validator.bornAfter1900, 'dob', dobObj, 'Enter a real date of birth'),
     ],
@@ -95,4 +49,13 @@ module.exports.validations = (req) => {
       new ValidationRule(validator.notEmpty, 'issuingState', req.body.issuingState, 'Enter the issuing state for the document'),
     ],
   ];
+
+  if (docTypeOther === 'Other') {
+    peopleValidationRules.push([
+      new ValidationRule(validator.notEmpty, 'travelDocumentOther', req.body.travelDocumentOther, 'Enter the document type you are using'),
+      new ValidationRule(validator.validName, 'travelDocumentOther', req.body.travelDocumentOther, 'Enter a real document type'),
+    ]);
+  }
+  
+  return peopleValidationRules;
 };
