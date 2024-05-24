c5b2249adad7f4ad75f17271c53deeadb63ccd38
Alastair <alastair.mottram-epson@digital.homeoffice.gov.uk>
2024-05-24 15:54:44 +0100

    sGAR ~ 2024.05.24 ~ removed stupid logs.

--- src/app/garfile/manifest/get.controller.js
+++ src/app/garfile/manifest/get.controller.js
@@ -93,7 +93,6 @@ module.exports = async (req, res) => {
     
       if (req.session.manifestErr) {
         const { manifestErr, manifestInvalidPeople } = req.session;
-        logger.error(JSON.stringify(manifestErr))
         delete req.session.manifestErr;
         delete req.session.manifestInvalidPeople;
         return res.render('app/garfile/manifest/index', { 
