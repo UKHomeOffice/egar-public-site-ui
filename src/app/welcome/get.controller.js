df54b30d5b5fbbabe2c1c470b408a08003acc0cc
Sarita More <saritamore@hod244003.local>
2024-06-25 16:39:13 +0100

    FAIR-524 added new environment variable HOMPAGE_MESSAGE

--- src/app/welcome/get.controller.js
+++ src/app/welcome/get.controller.js
@@ -1,6 +1,10 @@
 const logger = require('../../common/utils/logger')(__filename);
+const homePageMessageFromEnv = process.env.HOMEPAGE_MESSAGE;
+//After decryption of HOMEPAGE_MESSAGE evironment variable, the banner message displays with enclosed double quotes. 
+//Remove the duble quotes from start and end of the homePageMessageFromEnv string"
+const homePageMessage = homePageMessageFromEnv ? homePageMessageFromEnv.substring(1, homePageMessageFromEnv.length-1) : null;
 
 module.exports = (req, res) => {
   logger.debug('In welcome get controller');
-  res.render('app/welcome/index');
+  res.render('app/welcome/index', {homePageMessage});
 };
