514c33ffa162a8c85a814642f2e3173b8ba9d97e
Sarita More <saritamore@hod244003.local>
2024-06-25 16:39:13 +0100

    FAIR-524 added new environment variable HOMPAGE_MESSAGE

--- src/app/welcome/get.controller.js
+++ src/app/welcome/get.controller.js
@@ -1,6 +1,7 @@
 const logger = require('../../common/utils/logger')(__filename);
+const homePageMessage = process.env.HOMEPAGE_MESSAGE;
 
 module.exports = (req, res) => {
   logger.debug('In welcome get controller');
-  res.render('app/welcome/index');
+  res.render('app/welcome/index', {homePageMessage});
 };
