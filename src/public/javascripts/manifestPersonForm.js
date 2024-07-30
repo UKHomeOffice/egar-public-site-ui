4261fbf74d8e5c15ee61a387684dfd5d652e2150
Alastair <alastair.mottram-epson@digital.homeoffice.gov.uk>
2024-07-30 15:53:54 +0100

    FAIR-484 ~ 2024.07.30 ~ frontend isle of man haiddenign should done is h, now for validaiton and adaption.

---
+++ src/public/javascripts/manifestPersonForm.js
@@ -0,0 +1,21 @@
+const isIsleOfManFlight = document.getElementById('isIsleOfManFlight');
+const travelDocumentDetails = document.getElementById('travelDocumentDetails');
+const nationality = document.getElementById('nationality');
+const nationalitySelect = () => document.getElementById('nationality-select');
+
+const UK_COUNTRY_CODE = 'GBR';
+const IRELAND_COUNTRY_CODE = 'IRL';
+
+function hideTravelDocumentDetails(nationalityCountryCode) {
+  return  isIsleOfManFlight.value === 'true' && (
+    nationalityCountryCode === ''
+        || nationalityCountryCode === UK_COUNTRY_CODE
+        || nationalityCountryCode === IRELAND_COUNTRY_CODE
+  );
+}
+
+travelDocumentDetails.hidden = hideTravelDocumentDetails(nationalitySelect().value);
+
+nationality.addEventListener("change", (e) => {
+    travelDocumentDetails.hidden = hideTravelDocumentDetails(nationalitySelect().value);
+})
