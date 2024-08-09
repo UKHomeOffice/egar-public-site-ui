dd1199121035aa68b8d9fb7ccf8bceaaa31f22bf
Sarita More <saritamore@hod244003.home>
2024-08-09 08:44:31 +0100

    added testcase for success and error messages

--- src/test/organisation/searchusers/get.controller.test.js
+++ src/test/organisation/searchusers/get.controller.test.js
@@ -57,122 +57,112 @@ describe('Organisation Search Users Get Controller', () => {
     });
   });
 
+  describe('api returns ok', () => {
+    let cookie;
 
-  it('should display error message if set', async () => {
-    req.session.errMsg = { message: 'Example Error Message' };
-      const cookie = new CookieModel(req);
-      orgApiStub.resolves(JSON.stringify({
-        items: [
-          { id: 'PERSON-1', first_name: 'PERSON-1-name', role: { name: 'User' } },
-          { id: 'PERSON-4', first_name: 'PERSON-1-name', role: { name: 'User' } },
-          { id: 'PERSON-5', first_name: 'PERSON-1-name', role: { name: 'User' } },
-        ],
+    const apiResponse = 
+      [
+        { id: 'PERSON-1', first_name: 'PERSON-1-name', role: { name: 'User' } },
+        { id: 'PERSON-4', first_name: 'PERSON-1-name', role: { name: 'User' } },
+        { id: 'PERSON-5', first_name: 'PERSON-3-name', role: { name: 'Admin' } },
+      ];
+
+    beforeEach(() => {
+      cookie = new CookieModel(req);
+      cookie.setOrganisationUsers(apiResponse);
+      orgApiStub.resolves(JSON.stringify(apiResponse));     
+    });
+
+    it('should display error message if set', async () => {
+      req.session.errMsg = { message: 'Example Error Message' };
+
+      const callController = async () => {
+          await controller(req, res);
+        };
+
+      callController().then(() => {
+        expect(req.session.errMsg).to.be.undefined;
+        expect(orgApiStub).to.have.been.calledOnceWithExactly('ORGANISATION-ID-1', 'TEST' );
+        expect(res.render).to.not.have.been.calledOnceWithExactly('/organisation/index', {
+          cookie,
+          orgUser: [
+            { id: 'PERSON-1', first_name: 'PERSON-1-name', role: { name: 'User' } },
+            { id: 'PERSON-4', first_name: 'PERSON-1-name', role: { name: 'User' } },
+            { id: 'PERSON-5', first_name: 'PERSON-3-name', role: { name: 'Admin' } },
+          ],
+          errors: [{ message: 'Example error message' }]
+        });
         
-      }));
+      });
+    });
 
-    const callController = async () => {
+    it('should display success message if set', async () => {
+      req.session.successMsg = 'Example Success Message';
+      req.session.successHeader = 'Successful Header';
+      
+      const callController = async () => {
         await controller(req, res);
       };
 
-    callController().then(() => {
-      expect(req.session.errMsg).not.to.be.undefined;
-      expect(orgApiStub).to.have.been.calledOnceWithExactly('ORGANISATION-ID-1', 'TEST' );
-      expect(res.render).to.not.have.been.calledOnceWithExactly('/organisation/index', {
-        cookie,
-        orgUser: [
-          { id: 'PERSON-1', first_name: 'PERSON-1-name', role: { name: 'User' } },
-          { id: 'PERSON-4', first_name: 'PERSON-1-name', role: { name: 'User' } },
-          { id: 'PERSON-5', first_name: 'PERSON-3-name', role: { name: 'Admin' } },
-        ],
-        errors: [{ message: 'Example error message' }]
-      });
-      
-    });
-  });
-
-
-  it('should display success message if set', async () => {
-    req.session.successMsg = 'Example Success Message';
-    req.session.successHeader = 'Successful Header';
-    const cookie = new CookieModel(req);
-
-    orgApiStub.resolves(JSON.stringify({
-      items: [
-        { id: 'PERSON-1', first_name: 'PERSON-1-name', role: { name: 'User' } },
-        { id: 'PERSON-4', first_name: 'PERSON-1-name', role: { name: 'User' } },
-        { id: 'PERSON-5', first_name: 'PERSON-3-name', role: { name: 'Admin' } },
-      ],
-    }));
-    const callController = async () => {
-      await controller(req, res);
-    };
-
-    callController().then(() => {
-      expect(req.session.successMsg).not.to.be.undefined;
-      expect(req.session.successHeader).not.to.be.undefined;
-      expect(orgApiStub).to.have.been.calledOnceWithExactly('ORGANISATION-ID-1','TEST');
-      expect(res.render).to.not.have.been.calledOnceWithExactly('/organisation/index', {
-        cookie,
-        orgUser: [
-          { id: 'PERSON-1', first_name: 'PERSON-1-name', role: { name: 'User' } },
-          { id: 'PERSON-4', first_name: 'PERSON-1-name', role: { name: 'User' } },
-          { id: 'PERSON-5', first_name: 'PERSON-3-name', role: { name: 'Admin' } },
-        ],
-        successHeader: 'Successful Header',
-        successMsg: 'Example Success Message'
-      });
-      
-    });
-  });
-
-  it('should render with no users if no match', () => {
-    const cookie = new CookieModel(req);
-
-    orgApiStub.resolves(JSON.stringify({
-      items: [
-        { id: 'PERSON-1', first_name: 'PERSON-1-name', role: { name: 'User' } },
-        { id: 'PERSON-4', first_name: 'PERSON-1-name', role: { name: 'User' } },
-        { id: 'PERSON-5', first_name: 'PERSON-1-name', role: { name: 'User' } },
-      ],
-    }));
-    const callController = async () => {
-      await controller(req, res);
-    };
-
-    callController().then(() => {
-      expect(res.render).to.not.have.been.calledOnceWithExactly('/organisation/index', {
-        cookie,
-        orgUser: [],  
-      });
-      expect(orgApiStub).to.have.been.calledOnceWithExactly('ORGANISATION-ID-1', 'TEST' );
-    });
-  });
-
-  it('should render with users if matches', () => {
-    const cookie = new CookieModel(req);
-
-    orgApiStub.resolves(JSON.stringify({
-      items: [
-        { id: 'PERSON-1', first_name: 'PERSON-1-name', role: { name: 'User' } },
-        { id: 'PERSON-4', first_name: 'PERSON-1-name', role: { name: 'User' } },
-        { id: 'PERSON-5', first_name: 'PERSON-3-name', role: { name: 'Admin' } },
-      ],
-    }));
-    const callController = async () => {
-      await controller(req, res);
-    };
-
-    callController().then(() => {
-      expect(res.render).to.not.have.been.calledOnceWithExactly('/organisation/index', {
-        cookie,
-        orgUser: [
-          { id: 'PERSON-2', first_name: 'PERSON-2-name', role: 'User' },
-          { id: 'PERSON-5', first_name: 'PERSON-2-name', role: 'Admin' },
-        ],
+      callController().then(() => {
+        expect(req.session.successMsg).to.be.undefined;
+        expect(req.session.successHeader).to.be.undefined;
+        expect(orgApiStub).to.have.been.calledOnceWithExactly('ORGANISATION-ID-1','TEST');
+        expect(res.render).to.not.have.been.calledOnceWithExactly('/organisation/index', {
+          cookie,
+          orgUser: [
+            { id: 'PERSON-1', first_name: 'PERSON-1-name', role: { name: 'User' } },
+            { id: 'PERSON-4', first_name: 'PERSON-1-name', role: { name: 'User' } },
+            { id: 'PERSON-5', first_name: 'PERSON-3-name', role: { name: 'Admin' } },
+          ],
+          successHeader: 'Successful Header',
+          successMsg: 'Example Success Message'
+        });
         
       });
-      expect(orgApiStub).to.have.been.calledOnceWithExactly('ORGANISATION-ID-1','TEST');
     });
+
+    it('should render with no users if no match', () => {
+      const cookie = new CookieModel(req);
+
+      const callController = async () => {
+        await controller(req, res);
+      };
+
+      callController().then(() => {
+        expect(req.session.errMsg).to.be.undefined;
+        expect(req.session.successHeader).to.be.undefined;
+        expect(req.session.successMsg).to.be.undefined;
+        expect(res.render).to.not.have.been.calledOnceWithExactly('/organisation/index', {
+          cookie,
+          orgUser: [],  
+        });
+        expect(orgApiStub).to.have.been.calledOnceWithExactly('ORGANISATION-ID-1', 'TEST' );
+      });
+    });
+
+    it('should render with users if matches', () => {
+      const cookie = new CookieModel(req);
+
+      const callController = async () => {
+        await controller(req, res);
+      };
+
+      callController().then(() => {
+        expect(res.render).to.not.have.been.calledOnceWithExactly('/organisation/index', {
+          cookie,
+          orgUser: [
+            { id: 'PERSON-2', first_name: 'PERSON-2-name', role: 'User' },
+            { id: 'PERSON-5', first_name: 'PERSON-2-name', role: 'Admin' },
+          ],
+          
+        });
+        expect(orgApiStub).to.have.been.calledOnceWithExactly('ORGANISATION-ID-1','TEST');
+
+      });
+    });
+
   });
+
 });
 
