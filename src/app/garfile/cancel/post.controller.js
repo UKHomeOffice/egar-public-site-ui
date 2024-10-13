fcb2cbfb6873ef3875f7b45e58a77f6c9f6f6847
Alastair <alastair.mottram-epson@digital.homeoffice.gov.uk>
2024-10-13 18:52:32 +0100

    NMSW-2388 ~ 2024.10.13 ~ exception if the sqs queue where to go down, has been handled properly.

--- src/app/garfile/cancel/post.controller.js
+++ src/app/garfile/cancel/post.controller.js
@@ -8,48 +8,60 @@ module.exports = (req, res) => {
   logger.debug('In garfile / cancel post controller');
   const cookie = new CookieModel(req);
   const previousGarStatus = cookie.getGarStatus();
-    garApi.patch(cookie.getGarId(), 'Cancelled', {})
-      .then(() => {
-        if (!cookie.getCbpId()) {
-          req.session.successMsg = 'The GAR has been successfully cancelled';
-          req.session.successHeader = 'Cancellation Confirmation';
-          req.session.save(() => {
-            res.redirect('/home');
-          });
 
-          return;
-        }
+  const sendCancellationEmail = () => {
+    emailService.send(config.NOTIFY_GAR_CANCEL_TEMPLATE_ID, cookie.getUserEmail(), {
+      firstName: cookie.getUserFirstName(),
+      cancellationReference: cookie.getCbpId(),
+    }).then(() => {
+      req.session.successMsg = 'The GAR has been successfully cancelled';
+      req.session.successHeader = 'Cancellation Confirmation';
+      req.session.save(() => {
+        res.redirect('/home');
+      });
+    }).catch((err) => {
+      logger.error(err);
+      req.session.successMsg = 'The GAR has been successfully cancelled, but there was a problem with sending the email';
+      req.session.successHeader = 'Cancellation Confirmation';
+      req.session.save(() => {
+        res.redirect('/home');
+      });
+    });
+  };
 
-        if (previousGarStatus === "Submitted") {
-          garApi.postGarDepartureExceptions(cookie.getGarId()).catch((err) => {
-            logger.error('Api failed to post GAR exceptions');
-            logger.error(err);
-            res.render('app/home', {
-              cookie,
-              errors: [{
-                  message: 'Failed to raise departure exception in GAR.',
-              }],
-            });
-          });
-        }
+  garApi.patch(cookie.getGarId(), 'Cancelled', {})
+    .then(() => {
+      if (!cookie.getCbpId()) {
+        req.session.successMsg = 'The GAR has been successfully cancelled';
+        req.session.successHeader = 'Cancellation Confirmation';
+        req.session.save(() => {
+          res.redirect('/home');
+        });
 
-        emailService.send(config.NOTIFY_GAR_CANCEL_TEMPLATE_ID, cookie.getUserEmail(), {
-          firstName: cookie.getUserFirstName(),
-          cancellationReference: cookie.getCbpId(),
-        }).then(() => {
-          req.session.successMsg = 'The GAR has been successfully cancelled';
-          req.session.successHeader = 'Cancellation Confirmation';
-          req.session.save(() => {
-            res.redirect('/home');
-          });
-        }).catch((err) => {
-          req.session.successMsg = 'The GAR has been successfully cancelled, but there was a problem with sending the email';
-          req.session.successHeader = 'Cancellation Confirmation';
-          req.session.save(() => {
-            res.redirect('/home');
+        return;
+      }
+
+      if (previousGarStatus === 'Submitted') {
+        garApi.postGarDepartureExceptions(cookie.getGarId())
+        .then(() => {
+          sendCancellationEmail();
+        })
+        .catch((err) => {
+          logger.error('Api failed to post GAR exceptions');
+          logger.error(err);
+          return res.render('app/home', {
+            cookie,
+            errors: [{
+                message: 'Failed to raise departure exception in GAR.',
+            }],
           });
         });
-      })
+
+        return;
+      }
+
+      sendCancellationEmail();
+    })
     .catch((err) => {
       logger.error(err);
       res.render('app/garfile/cancel', { cookie, error: [{ message: 'Failed to cancel GAR' }] });
