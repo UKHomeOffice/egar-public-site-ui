63385709ae9995b7830079a31bdc6e199e3d7963
Sarita More <saritamore@hod244003.home>
2025-04-16 10:49:25 +0100

    NMSW-2868 - test GARs Submitted tab reload issue

--- src/app/home/get.controller.js
+++ src/app/home/get.controller.js
@@ -3,7 +3,7 @@ const CookieModel = require('../../common/models/Cookie.class');
 const tokenApi = require('../../common/services/tokenApi');
 const garApi = require('../../common/services/garApi');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   logger.debug('In register / reguser get controller');
   const cookie = new CookieModel(req);
 
@@ -13,39 +13,43 @@ module.exports = (req, res) => {
 
   // Delete any GAR stored in the cookie session
   cookie.session.gar = null;
-
-  tokenApi.getLastLogin(cookie.getUserEmail())
-    .then((userSession) => {
-      const { successHeader, successMsg } = req.session;
-      delete req.session.successHeader;
-      delete req.session.successMsg;
-
-      garApi.getGars(userId, role, orgId)
-        .then((apiResponse) => {
-          const garList = JSON.parse(apiResponse).items;
-          const draftGars = garList.filter(gar => gar.status.name === 'Draft');
-          const submittedGars = garList.filter(gar => gar.status.name === 'Submitted');
-          const cancelledGars = garList.filter(gar => gar.status.name === 'Cancelled');
-          res.render('app/home/index', {
-            cookie,
-            userSession,
-            successMsg,
-            successHeader,
-            draftGars,
-            submittedGars,
-            cancelledGars
-          });
-        })
-        .catch((err) => {
-          logger.error('Failed to get GARS from API');
-          logger.error(err);
-          res.render('app/home/index', {
-            cookie, successMsg, successHeader, errors: [{ message: 'Failed to get GARs' }],
-          });
-        });
-    })
-    .catch((err) => {
+  try {
+    const userSession = await tokenApi.getLastLogin(cookie.getUserEmail());
+    if (!userSession) {
+      logger.error('Failed to get GARS from API');
       logger.error(err);
       return res.render('app/home/index', { cookie, userSession: [] });
-    });
+    }
+
+    const { successHeader, successMsg } = req.session;
+    delete req.session.successHeader;
+    delete req.session.successMsg;
+    
+    try {
+      const apiResponse = await garApi.getGars(userId, role, orgId);
+      const garList = JSON.parse(apiResponse).items;
+      const draftGars = garList.filter(gar => gar.status.name === 'Draft');
+      const submittedGars = garList.filter(gar => gar.status.name === 'Submitted');
+      const cancelledGars = garList.filter(gar => gar.status.name === 'Cancelled');
+      res.render('app/home/index', {
+        cookie,
+        userSession,
+        successMsg,
+        successHeader,
+        draftGars,
+        submittedGars,
+        cancelledGars
+      });
+
+    } catch (err) {
+      logger.error('Failed to get GARS from API');
+      logger.error(err);
+      res.render('app/home/index', {
+        cookie, successMsg, successHeader, errors: [{ message: 'Failed to get GARs' }],
+      });
+    }
+  } catch (err) {
+    logger.error(err);
+    return res.render('app/home/index', { cookie, userSession: [] });
+  }
 };
