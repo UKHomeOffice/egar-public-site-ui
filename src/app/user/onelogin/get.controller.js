46f322bb059823b37542cf41e2571b24c1e65b08
ndifrekeekottHO <Ndifreke.Ekott@digital.homeoffice.gov.uk>
2025-05-08 17:19:11 +0100

    chore: Tidying up code

--- src/app/user/onelogin/get.controller.js
+++ src/app/user/onelogin/get.controller.js
@@ -6,17 +6,17 @@ const {PHASE_GIVEN_NAME, WORKFLOW_STEPS}  = require('./constants');
 module.exports = (req, res) => {
   let stepValue = req.session.step || PHASE_GIVEN_NAME;
 
+  if (!WORKFLOW_STEPS.includes(stepValue)) {
+    return res.redirect('error/404');
+  }
+
   if (req.query.action === 'change-name') {
     stepValue = PHASE_GIVEN_NAME;
   }
 
-
-  if (!WORKFLOW_STEPS.includes(stepValue)) {
-    return res.redirect('error/404');
-  }
   req.session.step = stepValue;
-  const stepData = req.session?.step_data;
-  console.log(stepData);
   req.session.save();
+
+  const stepData = req.session?.step_data;
   return res.render('app/user/onelogin/index', {step: `app/user/onelogin/partials/${stepValue}.njk`, ...stepData});
 };
