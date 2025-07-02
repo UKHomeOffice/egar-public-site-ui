68bbba14b3db4c7c31e28a7af1466ad487f9a3ce
ndifrekeekottHO <Ndifreke.Ekott@digital.homeoffice.gov.uk>
2025-07-02 11:50:19 +0100

    feat: Update reference to onelogin change email

--- src/app/user/viewDetails/get.controller.js
+++ src/app/user/viewDetails/get.controller.js
@@ -2,6 +2,7 @@ const CookieModel = require('../../../common/models/Cookie.class');
 const logger = require('../../../common/utils/logger')(__filename);
 const personApi = require('../../../common/services/personApi');
 const config = require('../../../common/config');
+const {ONE_LOGIN_ACCOUNT_URL} = require("../../../common/config");
 
 const template = config.ONE_LOGIN_SHOW_ONE_LOGIN === true ? 'index' : 'old_index'
 
@@ -18,7 +19,7 @@ module.exports = (req, res) => {
         const { errMsg } = req.session;
         delete req.session.errMsg;
         return res.render(`app/user/viewDetails/${template}`, {
-          cookie, savedPeople, errors: [errMsg]
+          cookie, savedPeople, errors: [errMsg], ONE_LOGIN_ACCOUNT_URL
         });
       }
       if (req.session.successMsg) {
@@ -26,7 +27,7 @@ module.exports = (req, res) => {
         delete req.session.successHeader;
         delete req.session.successMsg;
         return res.render(`app/user/viewDetails/${template}`, {
-          cookie, savedPeople, successHeader, successMsg,
+          cookie, savedPeople, successHeader, successMsg, ONE_LOGIN_ACCOUNT_URL
         });
       }
       return res.render(`app/user/viewDetails/${template}`, {cookie, savedPeople });
@@ -34,6 +35,6 @@ module.exports = (req, res) => {
     .catch((err) => {
       logger.error('There was an error fetching craft / people data for an individual');
       logger.error(err);
-      res.render(`app/user/viewDetails/${template}`, { cookie, errors: [{ message: 'There was a problem fetching data' }] });
+      res.render(`app/user/viewDetails/${template}`, { cookie, errors: [{ message: 'There was a problem fetching data' }], ONE_LOGIN_ACCOUNT_URL });
     });
 };
