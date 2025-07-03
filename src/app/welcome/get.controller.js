b5bd6dbb8e20cbed04366651437475ebd58bd34d
ndifrekeekottHO <Ndifreke.Ekott@digital.homeoffice.gov.uk>
2025-07-03 09:24:39 +0100

    feat: Add welcome screen post migration

--- src/app/welcome/get.controller.js
+++ src/app/welcome/get.controller.js
@@ -1,7 +1,13 @@
 const logger = require('../../common/utils/logger')(__filename);
 const { HOMEPAGE_MESSAGE } = require('../../common/config/index');
+const {ONE_LOGIN_POST_MIGRATION, ONE_LOGIN_ACCOUNT_URL} = require("../../common/config");
+const oneLoginUtil = require("../../common/utils/oneLoginAuth");
 
 module.exports = (req, res) => {
   logger.debug('In welcome get controller');
-  res.render('app/welcome/index', {HOMEPAGE_MESSAGE});
+  const oneLoginUrl = oneLoginUtil.getOneLoginAuthUrl(req, res)
+  let template = ONE_LOGIN_POST_MIGRATION === 'true' ? 'app/welcome/post_migration_page' : 'app/welcome/index';
+
+  console.log("Temoakte ", template, '   ', ONE_LOGIN_POST_MIGRATION, oneLoginUrl)
+  res.render(template, {HOMEPAGE_MESSAGE, ONE_LOGIN_POST_MIGRATION, oneLoginUrl: oneLoginUrl });
 };
