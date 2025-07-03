eb7b2e3a9d249e62393ac00078c5fd412f9d6d4d
Ndifreke Ekott <Ndifreke.Ekott@digital.homeoffice.gov.uk>
2025-07-03 17:11:28 +0100

    NMSW-3254-post-migration-login-screen (#602)
    * feat: Break screens into partials
    
    * feat: Add welcome screen post migration
    
    * delete old template
    
    * delete old template
    
    * feat: update spacing and copy
    
    ref: NMSQ-3254
    
    * feat: UI updates
    
    * test: fix mock for req
    
    * test: Add test for post migration screen
    
    * test: Add test for post migration screen
    
    * test: fix proxyrequire
    
    * test: fix proxyrequire

--- src/test/welcome/get.controller.test.js
+++ src/test/welcome/get.controller.test.js
@@ -4,30 +4,67 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
+const proxyrequire = require('proxyquire').noCallThru();
+const config = require('../../common/config/index');
+const oneLoginApi = require('../../common/utils/oneLoginAuth');
 
 require('../global.test');
 
-const controller = require('../../app/welcome/get.controller');
-
 describe('Welcome Get Controller', () => {
-  let req; let res;
+  let req; let res; let configMock;
 
   beforeEach(() => {
     chai.use(sinonChai);
 
-    req = {};
+    req = {
+      get: sinon.stub(),
+    };
     res = {
+      session: {cookies: {}},
+      cookie: sinon.spy(),
       render: sinon.spy(),
     };
+
+    configMock = {
+      ...config,
+      ONE_LOGIN_POST_MIGRATION: false,
+      ONE_LOGIN_SHOW_ONE_LOGIN: false,
+      HOMEPAGE_MESSAGE: 'Welcome to the new service',
+    }
   });
 
   afterEach(() => {
     sinon.restore();
+    configMock = {}
   });
 
   it('should render the welcome page', async () => {
+    const controller = proxyrequire('../../app/welcome/get.controller', {
+        '../../common/config/index': configMock,
+    });
+
     await controller(req, res);
 
     expect(res.render).to.have.been.calledWith('app/welcome/index');
+  });
+
+  it('should render the post migration page when flag is on', async () => {
+    configMock['ONE_LOGIN_POST_MIGRATION']  = true;
+    configMock['ONE_LOGIN_SHOW_ONE_LOGIN']  = false;
+
+    sinon.stub(oneLoginApi, 'getOneLoginAuthUrl').returns('https://onelogin.com');
+
+    const controller = proxyrequire('../../app/welcome/get.controller', {
+        '../../common/config/index': configMock,
+    });
+
+
+    await controller(req, res);
+
+    expect(res.render).to.have.been.calledWith('app/welcome/post_migration_page', {
+      HOMEPAGE_MESSAGE: 'Welcome to the new service',
+      ONE_LOGIN_POST_MIGRATION: true,
+      oneLoginUrl: 'https://onelogin.com',
+    });
   });
 });
