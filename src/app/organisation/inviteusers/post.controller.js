91cda2956cb8a991e7d4e3e2da7e1c6e0628e784
Sarita More <133763811+SaritaMore7@users.noreply.github.com>
2025-07-07 12:42:18 +0100

    Nmsw 3166 invite fail to individual user (#607)
    * NMSW-3158- added new the email template in config
    
    * NMSW-3166 added a check for email address, if it exist then send user to error page
    
    * NMSW-3166 added a testcase
    
    * NMSW-3166 updated error message content

--- src/app/organisation/inviteusers/post.controller.js
+++ src/app/organisation/inviteusers/post.controller.js
@@ -1,7 +1,9 @@
 const logger = require('../../../common/utils/logger')(__filename);
 const validator = require('../../../common/utils/validator');
 const validations = require('./validations');
+const userApi = require('../../../common/services/userManageApi');
 const CookieModel = require('../../../common/models/Cookie.class');
+const { response } = require('express');
 
 module.exports = (req, res) => {
   const { fname, lname, email } = req.body;
@@ -13,7 +15,20 @@ module.exports = (req, res) => {
       cookie.setInviteUserFirstName(fname);
       cookie.setInviteUserLastName(lname);
       cookie.setInviteUserEmail(email);
-      res.redirect('/organisation/assignrole');
+      userApi.getDetails(email)
+        .then((apiResponse) => {
+          if (apiResponse.message === 'User not registered') {
+            return res.redirect('/organisation/assignrole');
+          }
+          if (typeof apiResponse.email !== 'undefined' && apiResponse.email.toLowerCase() === email.toLowerCase()) {
+            res.render('app/organisation/inviteusers/userExistError');
+            return;
+          }
+        })
+        .catch((err) => {
+          logger.error(`Error: proble while getting a user details ${err}`);
+          return res.redirect('/organisation/inviteuser');
+        });
     })
     .catch((err) => {
       logger.error('Invite Users Organisation postcontroller - There was a problem inviting a user');
@@ -23,3 +38,5 @@ module.exports = (req, res) => {
       });
     });
 };
+
+
