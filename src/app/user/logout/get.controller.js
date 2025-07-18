634d6f75301d0377086fc5e69c7ed7079bfb0505
ndifrekeekottHO <Ndifreke.Ekott@digital.homeoffice.gov.uk>
2025-07-18 14:33:27 +0100

    revert changes

--- src/app/user/logout/get.controller.js
+++ src/app/user/logout/get.controller.js
@@ -18,13 +18,13 @@ module.exports = (req, res) => {
   let { state, id_token } = req.cookies || {};
 
   // If returning from Onelogin with a "user-deleted" state
-  if (req.query.state === 'user-deleted') {
+  if (req.query?.state === 'user-deleted') {
     return logoutAndClearCookies(req, res, cookie, '/user/deleteconfirm');
   }
 
   // If we detect valid OneLogin session cookies
   if (state && id_token) {
-    if (req.query.action === 'user-deleted') {
+    if (req.query?.action === 'user-deleted') {
       state = 'user-deleted';
     }
     const logoutUrl = getOneLoginLogoutUrl(req, id_token, state);
