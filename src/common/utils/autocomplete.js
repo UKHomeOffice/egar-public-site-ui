57afedb7d22df2ba7710805efd563f06d4344807
ndifrekeekottHO <Ndifreke.Ekott@digital.homeoffice.gov.uk>
2025-09-29 10:50:17 +0100

    feat: Filtering out nationalities

--- src/common/utils/autocomplete.js
+++ src/common/utils/autocomplete.js
@@ -8,11 +8,21 @@ const airportList = require('./airport_codes.json');
  * pair and using instead alpha-3 (GBR) as the key.
  */
 
+const COUNTRY_SKIP_LIST = [
+  'ABW', 'AIA', 'ALA', 'ANT', 'ASM', 'ATA', 'ATF', 'BES', 'BLM', 'BMU', 'BVT',
+  'CCK', 'COK', 'CUW', 'CXR', 'CYM', 'D', 'ESH', 'FLK', 'FRO', 'GGY', 'GIB',
+  'GLP', 'GRL', 'GUF', 'GUM', 'HMD', 'IMN', 'JEY', 'MAF', 'MID', 'MNP', 'MSR',
+  'MTQ', 'MYT', 'NCL', 'NFK', 'NIU', 'PCN', 'PRI', 'PSE', 'PYF', 'REU', 'SGS', 'SHN',
+  'SJM', 'SPM', 'SRB', 'SXM', 'TCA', 'TKL', 'UMI', 'VGB', 'VIR', 'WLF', 'XCT', 'XKK'
+]
+
 // ISO/IEC 7501-1 codes, not part of ISO 3166/MA
 const customNationalities = [
   { code: 'GBD', label: 'British Overseas Territories Citizen (GBD)' },
   { code: 'GBN', label: 'British National (Overseas) (GBN)' },
   { code: 'GBO', label: 'British Overseas Citizen (GBO)' },
+  { code: 'RKS', label: 'Kosovo' },
+  { code: 'PSE', label: 'Palestine Authority' },
 ];
 
 const generateNationalityList = () => {
@@ -27,6 +37,12 @@ const generateNationalityList = () => {
 
   Object.keys(countries.getNames('en')).forEach((key) => {
     const alpha3 = countries.alpha2ToAlpha3(key);
+
+    if (COUNTRY_SKIP_LIST.includes(alpha3)) {
+      console.log(`Skipping ${alpha3} as it is in the skip list`);
+      return;
+    }
+
     const countryName = countries.getNames('en')[key];
     alpha3List.push({ code: alpha3, label: `${countryName} (${alpha3})` });
   });
