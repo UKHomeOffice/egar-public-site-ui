405d068483632d7a61e6ef9875f4aa25a7dd72ba
Henry Senior <112489564+ho-henry-senior@users.noreply.github.com>
2025-10-17 21:53:52 +0100

    nmsw-3807: drone pipeline updates. (#676)
    * nmsw-3807: drone pipeline updates.
    
    * nmsw-3807: ...
    
    * nmsw-3807: split build and push into separate pipeline steps.
    
    * nmsw-3807: shell refactor.

--- ops/image_push.sh
+++ ops/image_push.sh
@@ -5,6 +5,5 @@ echo "echo docker now available."
 apk --update add --no-cache aws-cli
 export ECR_PASSWORD=$(aws ecr get-login-password --region eu-west-2 --output text)
 docker login -u AWS -p $ECR_PASSWORD ${DOCKER_REGISTRY_URL}
-docker build -t egar_ui-public-site:${DRONE_COMMIT} .
-docker tag egar_ui-public-site:${DRONE_COMMIT} ${REPO}:${DRONE_COMMIT}
+docker tag egar-public-site-ui:${DRONE_COMMIT} ${REPO}:${DRONE_COMMIT}
 docker push ${REPO}:${DRONE_COMMIT}
