f0c368221f1e885c28b24e42379f5d64812be7a2
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-10-17 21:52:12 +0100

    nmsw-3807: shell refactor.

--- ops/image_build.sh
+++ ops/image_build.sh
@@ -2,4 +2,4 @@
 echo "Checking for docker availability."
 n=0; while [ "$n" -lt 60 ] && ! docker stats --no-stream >/dev/null 2>&1; do n=$(( n + 1 )); sleep 1; done
 echo "echo docker now available."
-docker build -t egar_ui-public-site:${DRONE_COMMIT} .
+docker build -t egar-public-site-ui:${DRONE_COMMIT} .
--- ops/image_push.sh
+++ ops/image_push.sh
@@ -1,7 +1,9 @@
 # Wait for the docker service to be up
 echo "Checking for docker availability."
 n=0; while [ "$n" -lt 60 ] && ! docker stats --no-stream >/dev/null 2>&1; do n=$(( n + 1 )); sleep 1; done
+echo "echo docker now available."
 apk --update add --no-cache aws-cli
 export ECR_PASSWORD=$(aws ecr get-login-password --region eu-west-2 --output text)
 docker login -u AWS -p $ECR_PASSWORD ${DOCKER_REGISTRY_URL}
+docker tag egar-public-site-ui:${DRONE_COMMIT} ${REPO}:${DRONE_COMMIT}
 docker push ${REPO}:${DRONE_COMMIT}
