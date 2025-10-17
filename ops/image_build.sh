32c4dc7762d94f575354c8849f255b277ba9c1e1
Henry Senior <112489564+ho-henry-senior@users.noreply.github.com>
2025-10-17 21:59:35 +0100

    Dev (#677)
    * test pipeline
    
    * nmsw-3807: drone pipeline updates. (#676)
    
    * nmsw-3807: drone pipeline updates.
    
    * nmsw-3807: ...
    
    * nmsw-3807: split build and push into separate pipeline steps.
    
    * nmsw-3807: shell refactor.

---
+++ ops/image_build.sh
@@ -0,0 +1,5 @@
+# Wait for the docker service to be up
+echo "Checking for docker availability."
+n=0; while [ "$n" -lt 60 ] && ! docker stats --no-stream >/dev/null 2>&1; do n=$(( n + 1 )); sleep 1; done
+echo "echo docker now available."
+docker build -t egar-public-site-ui:${DRONE_COMMIT} .
