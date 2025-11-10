c56f5c6c30b659c579e92d0c9ec762b7e0d66a0f
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-10 20:02:21 +0000

    nmsw-3969: linting fix.

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -5,61 +5,61 @@ import eslintPluginPrettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
-    {
-        files: ['**/*.{js,cjs,mjs}'],
-        plugins: {
-            js,
-            prettier: eslintPluginPrettier,
-        },
-        extends: [
-            'js/recommended',
-            prettierConfig, // disables rules conflicting with Prettier
-        ],
-        rules: {
-            'prettier/prettier': 'error', // Treat Prettier issues as ESLint errors
-            'max-len': 'off', // Prettier handles line length
-            'unicorn/prefer-module': 'off', // allow require()
-            'unicorn/prevent-abbreviations': 'off', // optional
-        },
-        languageOptions: {
-            globals: {
-                ...globals.node,
-            },
-        },
+  {
+    files: ['**/*.{js,cjs,mjs}'],
+    plugins: {
+      js,
+      prettier: eslintPluginPrettier,
     },
-    {
-        files: ['public/javascripts/**/*.js'],
-        languageOptions: {
-            globals: {
-                ...globals.browser, // window, document, etc.
-                $: 'readonly', // jQuery global
-                jQuery: 'readonly',
-            },
-        },
-        rules: {
-            'no-unused-vars': ['warn', { args: 'none' }],
-            'no-undef': 'off',
-            'no-console': 'off',
-            'no-alert': 'off',
-        },
-        ignores: ['public/javascripts/all.js'],
+    extends: [
+      'js/recommended',
+      prettierConfig, // disables rules conflicting with Prettier
+    ],
+    rules: {
+      'prettier/prettier': 'error', // Treat Prettier issues as ESLint errors
+      'max-len': 'off', // Prettier handles line length
+      'unicorn/prefer-module': 'off', // allow require()
+      'unicorn/prevent-abbreviations': 'off', // optional
     },
-    {
-        files: ['**/*.js'],
-        languageOptions: {
-            sourceType: 'commonjs',
-        },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
     },
-    {
-        files: ['test/**/*.js'],
-        languageOptions: {
-            globals: {
-                ...globals.mocha,
-            },
-        },
-        rules: {
-            'n/no-unpublished-require': 'off', // devDeps in tests
-        },
+  },
+  {
+    files: ['public/javascripts/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser, // window, document, etc.
+        $: 'readonly', // jQuery global
+        jQuery: 'readonly',
+      },
     },
-    prettierConfig,
+    rules: {
+      'no-unused-vars': ['warn', { args: 'none' }],
+      'no-undef': 'off',
+      'no-console': 'off',
+      'no-alert': 'off',
+    },
+    ignores: ['public/javascripts/all.js'],
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+  },
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+    rules: {
+      'n/no-unpublished-require': 'off', // devDeps in tests
+    },
+  },
+  prettierConfig,
 ]);
