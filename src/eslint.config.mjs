7a97ab718a36b3a7c6b81fa1243c6e106986ccef
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-10 20:14:38 +0000

    nmsw-3969: linting fix.

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -5,68 +5,62 @@ import eslintPluginPrettier from 'eslint-plugin-prettier';
 import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
-    {
-        files: ['**/*.{js,cjs,mjs}'],
-        ignores: [
-            'public/javascripts/all.js',
-            '**/*.min.js',
-        ],
-        plugins: {
-            js,
-            prettier: eslintPluginPrettier,
-        },
-        extends: [
-            'js/recommended',
-            prettierConfig, // disables rules conflicting with Prettier
-        ],
-        rules: {
-            'prettier/prettier': 'error', // Treat Prettier issues as ESLint errors
-            'max-len': 'off', // Prettier handles line length
-            'unicorn/prefer-module': 'off', // allow require()
-            'unicorn/prevent-abbreviations': 'off', // optional
-        },
-        languageOptions: {
-            globals: {
-                ...globals.node,
-            },
-        },
+  {
+    files: ['**/*.{js,cjs,mjs}'],
+    ignores: ['public/javascripts/all.js', '**/*.min.js'],
+    plugins: {
+      js,
+      prettier: eslintPluginPrettier,
     },
-    {
-        files: ['public/javascripts/**/*.js'],
-        languageOptions: {
-            globals: {
-                ...globals.browser, // window, document, etc.
-                $: 'readonly', // jQuery global
-                jQuery: 'readonly',
-            },
-        },
-        rules: {
-            'no-unused-vars': ['warn', { args: 'none' }],
-            'no-undef': 'off',
-            'no-console': 'off',
-            'no-alert': 'off',
-        },
-        ignores: [
-            'public/javascripts/all.js',
-            '**/*.min.js',
-        ],
+    extends: [
+      'js/recommended',
+      prettierConfig, // disables rules conflicting with Prettier
+    ],
+    rules: {
+      'prettier/prettier': 'error', // Treat Prettier issues as ESLint errors
+      'max-len': 'off', // Prettier handles line length
+      'unicorn/prefer-module': 'off', // allow require()
+      'unicorn/prevent-abbreviations': 'off', // optional
     },
-    {
-        files: ['app/**/*.js'],
-        languageOptions: {
-            sourceType: 'commonjs',
-        },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
     },
-    {
-        files: ['test/**/*.js'],
-        languageOptions: {
-            globals: {
-                ...globals.mocha,
-            },
-        },
-        rules: {
-            'n/no-unpublished-require': 'off', // devDeps in tests
-        },
+  },
+  {
+    files: ['public/javascripts/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser, // window, document, etc.
+        $: 'readonly', // jQuery global
+        jQuery: 'readonly',
+      },
     },
-    prettierConfig,
+    rules: {
+      'no-unused-vars': ['warn', { args: 'none' }],
+      'no-undef': 'off',
+      'no-console': 'off',
+      'no-alert': 'off',
+    },
+    ignores: ['public/javascripts/all.js', '**/*.min.js'],
+  },
+  {
+    files: ['app/**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+  },
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+    rules: {
+      'n/no-unpublished-require': 'off', // devDeps in tests
+    },
+  },
+  prettierConfig,
 ]);
