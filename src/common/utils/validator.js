9141851003714cb979dce03820a058cff68696e4
Henry Senior <112489564+ho-henry-senior@users.noreply.github.com>
2025-11-11 12:00:50 +0000

    Revert "added entry mimeType=txt in validator.js so a normal text file can be uploded temporarly for test" (#733)
    This reverts commit d2c857b36f9213bef3b288d771d8fe34edec90fd.

--- src/common/utils/validator.js
+++ src/common/utils/validator.js
@@ -615,7 +615,6 @@ function isValidFileMime(fileName, mimeType) {
     gif: ['image/gif'],
     docx: ['application/vnd.openxmlformats-officedocument.wordprocessingml.document'],
     doc: ['application/msword'],
-    txt: ['application/text/plain'],
   };
   const fileExtension = fileName.split('.').slice(-1).pop();
   if (fileTypeObj[fileExtension]) {
