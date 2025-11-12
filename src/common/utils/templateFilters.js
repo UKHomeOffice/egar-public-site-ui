80116c7cb77449297fb7480595b3e6dfab688516
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-12 17:03:38 +0000

    bug fix.

--- src/common/utils/templateFilters.js
+++ src/common/utils/templateFilters.js
@@ -6,12 +6,15 @@ const uncamelCase = (value) => {
 
   return uncamelStr;
 };
-
+/*
+ * Check if there is an error in 'errors' whose identifier matches 'value'.
+ * If not, return false, if so return message in a format govuk macros recognise.
+ * */
 const containsError = (array, value) => {
   if (array === undefined || value === undefined) return false;
 
   const result = array.filter((element) => element.identifier === value);
-  return result.length > 0 && result[0];
+  return result.length > 0 && { text: result[0].message };
 };
 
 exports.uncamelCase = uncamelCase;
