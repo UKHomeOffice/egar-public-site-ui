da4c34874bb79ad4b246bb9cf2bbd88ac1837f99
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-12 17:06:59 +0000

    fixed unit tests.

--- src/test/common/utils/templateFilters.test.js
+++ src/test/common/utils/templateFilters.test.js
@@ -42,12 +42,10 @@ describe('Template Filters Utility', () => {
 
     it('should return element if found', () => {
       expect(templateFilters.containsError(exampleArray, 'firstName')).to.deep.equal({
-        identifier: 'firstName',
-        message: 'Not important',
+        text: 'Not important',
       });
       expect(templateFilters.containsError(exampleArray, 'surname')).to.deep.equal({
-        identifier: 'surname',
-        message: 'Does not matter',
+        text: 'Does not matter',
       });
     });
     it('should return truthy value if found', () => {
