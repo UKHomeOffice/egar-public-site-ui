6cd959982c80909936a19feeb0d33dd0e18a5c44
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-18 19:26:52 +0000

    fix copilot bug.

--- src/public/javascripts/people/add.js
+++ src/public/javascripts/people/add.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('travelDocumentType').addEventListener('change', checkDocumentType);
-  document.getElementById('add-and-exit').addEventListener('click', () => {
+  document.getElementById('add-and-exit').addEventListener('click', (event) => {
     sendAnalytics(event, 'Saved People Add - Save', 'click');
   });
-  document.getElementById('exit-without-adding').addEventListener('click', () => {
+  document.getElementById('exit-without-adding').addEventListener('click', (event) => {
     sendAnalytics(event, 'Saved People Add - Cancel', 'click');
   });
 });
