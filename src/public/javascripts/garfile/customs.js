4205ddc2d566232d4f1eaba16d417da598042d32
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-18 21:59:06 +0000

    move inline javascript out of garfile customs.

---
+++ src/public/javascripts/garfile/customs.js
@@ -0,0 +1,13 @@
+function hide() {
+  if (document.getElementById('Yes  ').checked) {
+    document.getElementById('prohibitedGoods').style.display = 'block';
+    document.getElementById('baggage').style.display = 'block';
+    document.getElementById('continentalShelf').style.display = 'block';
+  }
+  else {
+    document.getElementById('prohibitedGoods').style.display = 'none';
+    document.getElementById('baggage').style.display = 'none';
+    document.getElementById('continentalShelf').style.display = 'none';
+  }
+}
+document.addEventListener("DOMContentLoaded", hide);
