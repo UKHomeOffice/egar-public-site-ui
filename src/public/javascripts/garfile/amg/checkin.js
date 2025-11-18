b744ccaf0c7c26b88dc12f83a661e08cd98257f3
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-18 02:56:58 +0000

    fix garfile/amg/checkin

--- src/public/javascripts/garfile/amg/checkin.js
+++ src/public/javascripts/garfile/amg/checkin.js
@@ -20,6 +20,30 @@ document.addEventListener('DOMContentLoaded', () => {
   if (loadingScreen) {
     pollIntervalId = setInterval(pollAndUpdateDOM, pollingInterval);
   }
+  const resubmitted = $('#resubmitted').val();
+
+  window.myinterval = setInterval(function () {
+    const permissionDetails = () => Array.from(document.getElementsByClassName('permission-details'));
+    const permissionDetailsState = permissionDetails().map(el => el.open);
+
+    $.get(`/garfile/amg/checkin?template=pane&resubmitted=${resubmitted}`, null, function (data) {
+      const html = $(data);
+
+      if (!html.find('#spinner').length) {
+        clearInterval(window.myinterval);
+      }
+
+      $('#pane').html($(data));
+      $('#banner0T-content').appendTo('#top-notification-banner');
+      $('#banner0T').empty();
+
+      permissionDetails().forEach((el, index) => {
+
+        el.toggleAttribute("open", permissionDetailsState[index])
+      })
+    });
+
+  }, 3000);
 });
 
 $(document).on('click', '#resubmit0T', function () {
