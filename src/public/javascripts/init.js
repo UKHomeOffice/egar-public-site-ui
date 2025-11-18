8077edefb8e9c86f58d82f519276fbd3d994e12a
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-18 22:54:22 +0000

    reorganise js files to match directory structure.

---
+++ src/public/javascripts/init.js
@@ -0,0 +1,3 @@
+// document.body.className += ' js-enabled' + ('noModule' in HTMLScriptElement.prototype ? ' govuk-frontend-supported' : '');
+import { initAll } from '/javascripts/govuk-frontend.min.js'
+initAll()
