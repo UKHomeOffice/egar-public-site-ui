d54c3f2163e5ba2436e59f44151783aa911756aa
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-21 16:33:03 +0000

    remove inline event handler from verify/mfa.

---
+++ src/public/javascripts/verify/mfa.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('submit').addEventListener('click', (event) => {
+    sendAnalytics(event, 'Send MFA code - submit', 'click');
+  });
+});
+
+
