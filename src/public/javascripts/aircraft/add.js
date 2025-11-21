479c336ee2a835493af324f37eb8232f1e680326
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-21 16:22:21 +0000

    remove inline event handlers from aircraft/add.

---
+++ src/public/javascripts/aircraft/add.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('add-and-exit').addEventListener('click', (event) => {
+    sendAnalytics(event, 'Saved Aircraft Add - Save', 'click');
+  });
+  document.getElementById('exit-without-saving').addEventListener('click', (event) => {
+    sendAnalytics(event, 'Saved Aircraft Add - Cancel', 'click');
+  });
+});
+
+
