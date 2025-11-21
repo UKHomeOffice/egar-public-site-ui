019f4f427987bf3119ef2d5148ced7cf8d5b23f8
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-21 16:52:01 +0000

    remove inline event handler from garfile/manifest/addnewperson.

---
+++ src/public/javascripts/garfile/manifest/addnewperson.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('add-person-manifest').addEventListener('click', (event) => {
+    sendAnalytics(event, 'Save and Exit', 'click');
+  });
+});
+
+
