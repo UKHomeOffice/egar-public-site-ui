13162763e033d919a895a75915a9156ee20bfdc9
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-22 13:11:19 +0000

    nmsw-3975: uninstall unused package correlation-id; remove configuration and unit tests.

--- src/common/middleware/correlation-header.js
+++
@@ -1,8 +0,0 @@
-const config = require('../config/index');
-
-const { CORRELATION_HEADER } = config;
-
-module.exports = (req, _res, next) => {
-  req.correlationId = req.headers[CORRELATION_HEADER] || '';
-  next();
-};
