4bd72361d2faff82223fe3c9e7d9fedad5c9d00c
Henry Senior <henry.senior@digital.homeoffice.gov.uk>
2025-11-27 17:50:35 +0000

    fixed bug with validation errors for people dob and document exipiry date.

--- src/app/people/validations.js
+++ src/app/people/validations.js
@@ -8,11 +8,11 @@ module.exports.validations = (req) => {
   const docTypeOther = req.body.travelDocumentType;
 
   const peopleValidationRules = [
-    [new ValidationRule(validator.bornAfter1900, 'dob', dobObj, 'Enter a real date of birth')],
+    [new ValidationRule(validator.bornAfter1900, 'dobDate', dobObj, 'Enter a real date of birth')],
     [
       new ValidationRule(
         validator.realDateInFuture,
-        'documentExpiryDate',
+        'expiryDate',
         expiryDateObj,
         'Enter a real document expiry date'
       ),
