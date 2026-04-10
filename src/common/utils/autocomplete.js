d3cfa4b71479852ecf60a58898f65df6af1df3ba
Ndifreke Ekott <Ndifreke.Ekott@digital.homeoffice.gov.uk>
2026-04-10 16:08:16 +0100

    SGAR v13.34 (#811)
    * NMSW 4611 Added 3 new airfields
    
    * NMSW 4611 Added 3 new airfields
    
    * Add country reference
    
    * Removed hidden folder
    
    * Added airport list to csv file
    
    * NMSW-4629-SGAR---FE---Prepare-correct-port-list (#808)
    
    * feat: Revert to old airport list
    
    * feat: Remove label from borderforce code
    
    * revert: Revert debug code
    
    * Apply suggestion from @ndifrekeekottHO
    
    * NMSW-4629-revert-review-code (#809)
    
    * feat: Revert to old airport list
    
    * feat: Remove label from borderforce code
    
    * revert: Revert debug code
    
    * Apply suggestion from @ndifrekeekottHO
    
    * feat: Revert changes in check-answer
    
    - revert code in post controllers for arrival and depature
    - updated codeToLabel function
    
    * feat: Revert changes in check-answer
    
    - revert code in post controllers for arrival and depature
    - updated codeToLabel function
    
    * lint: Silence no-use-var warnings
    
    * revert: Revert test
    
    ---------
    
    Co-authored-by: TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
    Co-authored-by: TemitopeAyokuHO <138882912+TemitopeAyokuHO@users.noreply.github.com>

--- src/common/utils/autocomplete.js
+++ src/common/utils/autocomplete.js
@@ -1,18 +1,37 @@
 const nationalities = require('./nationality');
 const nationalityList = nationalities.nationalities();
 const countryList = nationalities.countries();
-const airportUtility = require('./airports');
+const airportList = require('./airport_codes.json');
 
 function getCountryFromCode(countryCode) {
   const country = nationalities.getByCode(countryCode);
   return country?.label ?? countryCode;
 }
 
-const airportList = airportUtility.allAirports();
+/**
+ * generate list of airport codes from the airport list
+ * @param {Array<Object>}
+ * @returns {Array<String>} List of airport codes
+ */
+function listOfAirportCodes(airportList) {
+  const airportCodes = [];
+
+  airportList.forEach((airport) => {
+    const iataCode = airport.id;
+    const icaoCode = airport.id2;
+    airportCodes.push(iataCode);
+    airportCodes.push(icaoCode);
+  });
+
+  return airportCodes;
+}
+
+const airportCodeList = listOfAirportCodes(airportList);
 
 module.exports = {
   getCountryFromCode,
   nationalityList,
   countryList,
   airportList,
+  airportCodeList,
 };
